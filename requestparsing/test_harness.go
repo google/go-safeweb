beb7c84c7cdc99cf6e86cfc160d3cc3f688f797c
Mara Mihali <maramihali@google.com>
2020-07-07 12:29:01 +0300

    Refactored testing harness documentation and renamed defined types from mocks to fakes.

--- requestparsing/test_harness.go
+++ requestparsing/test_harness.go
@@ -9,38 +9,41 @@ import (
 	"sync"
 )
 
-type mockHandler struct {
-	// called in the ServeHTTP function with the received request.
+type fakeHandler struct {
+	// callback is an anonymous function that verifies properties of the HTTP
+	// request and response provided in the test cases.
 	callback func(*http.Request)
 }
 
-func (h *mockHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (h *fakeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	h.callback(r)
 	io.WriteString(w, "Hello world!")
 }
 
-type mockListener struct {
+type fakeListener struct {
 	closeOnce      sync.Once
 	connChannel    chan net.Conn
 	serverEndpoint io.Closer
 	clientEndpoint net.Conn
 }
 
-// Creates a mock listener that passes requests to the HTTP server as part of
-// the test harness
-func newMockListener() *mockListener {
+// newFakeListener creates an instance of fakeListener. This will pass requests
+// to the HTTP server as part of the testing harness.
+func newFakeListener() *fakeListener {
 	s2c, c2s := net.Pipe()
 	c := make(chan net.Conn, 1)
 	c <- s2c
-	return &mockListener{
+	return &fakeListener{
 		connChannel:    c,
 		serverEndpoint: s2c,
 		clientEndpoint: c2s,
 	}
 }
 
-// Passes an endpoint to the server to enable communication to client
-func (l *mockListener) Accept() (net.Conn, error) {
+// Accept passes a network connection to the HTTP server to
+// enable bidirectional communication with the client. It will return an error
+// if Accept is called after the listener was closed
+func (l *fakeListener) Accept() (net.Conn, error) {
 	ch, ok := <-l.connChannel
 	if !ok {
 		return nil, errors.New("Listener closed")
@@ -48,7 +51,8 @@ func (l *mockListener) Accept() (net.Conn, error) {
 	return ch, nil
 }
 
-func (l *mockListener) Close() (err error) {
+// Close will close the two network connections and the listener
+func (l *fakeListener) Close() (err error) {
 	l.closeOnce.Do(func() {
 		err = l.serverEndpoint.Close()
 		if err != nil {
@@ -63,13 +67,15 @@ func (l *mockListener) Close() (err error) {
 	return err
 }
 
-func (l *mockListener) Addr() net.Addr {
+// Addr returns the network address of the client endpoint.
+func (l *fakeListener) Addr() net.Addr {
 	return l.clientEndpoint.LocalAddr()
 }
 
-// SendRequest writes 'request' to the clientEndpoint connection which will //send the request to the server listening on this listener.
-// Blocks until the server has read the message.
-func (l *mockListener) SendRequest(request []byte) error {
+// SendRequest writes a request to the client endpoint connection. This will
+// be passed to the server through the listener. The function blocks until the
+// server has finished reading the message.
+func (l *fakeListener) SendRequest(request []byte) error {
 	wrote, err := l.clientEndpoint.Write(request)
 	if requestLen := len(request); wrote != requestLen {
 		return errors.New("client connection failed to write the entire request")
@@ -77,10 +83,10 @@ func (l *mockListener) SendRequest(request []byte) error {
 	return err
 }
 
-// ReadResponse reads the response from the clientEndpoint connection which is
-// sent by the listening server. Blocks until the server has sent its response
-// or times out.
-func (l *mockListener) ReadResponse() ([]byte, error) {
+// readResponse reads the response from the clientEndpoint connection, sent by
+// the listening server. It will block until the server has sent a response
+// or has timed out.
+func (l *fakeListener) readResponse() ([]byte, error) {
 	// TODO(maramihali@, grenfeldt@): refactor this
 	bytes := make([]byte, 4096)
 	n, err := l.clientEndpoint.Read(bytes)
@@ -90,12 +96,13 @@ func (l *mockListener) ReadResponse() ([]byte, error) {
 	return bytes[:n], err
 }
 
+// makeRequest instantiates a new http.Server, sends the request provided as
+// argument and returns the response
 func makeRequest(ctx context.Context, req []byte, callbackFun func(*http.Request)) ([]byte, error) {
-
-	listener := newMockListener()
+	listener := newFakeListener()
 	defer listener.Close()
 
-	handler := &mockHandler{callback: callbackFun}
+	handler := &fakeHandler{callback: callbackFun}
 	server := &http.Server{Handler: handler}
 	go server.Serve(listener)
 	defer server.Close()
@@ -104,10 +111,10 @@ func makeRequest(ctx context.Context, req []byte, callbackFun func(*http.Request
 		return nil, err
 	}
 
-	respBody, err := listener.ReadResponse()
+	resp, err := listener.readResponse()
 	if err != nil {
 		return nil, err
 	}
 
-	return respBody, server.Shutdown(ctx)
+	return resp, server.Shutdown(ctx)
 }
