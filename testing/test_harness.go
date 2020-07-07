4bb15545f52c90b39732fc2ee90c7c2976d1d86e
Mara Mihali <maramihali@google.com>
2020-07-07 18:25:00 +0300

    Added README.md, made small refactorings to error-handling, renamed the package to testing and made the custom listener exportable

--- testing/test_harness.go
+++ testing/test_harness.go
@@ -1,19 +1,18 @@
-/*
-Copyright 2020 Google LLC
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-	https://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-package requestparsing
+package testing
 
 import (
 	"context"
@@ -34,7 +33,9 @@ func (h *AssertHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Hello world!")
 }
 
-type fakeListener struct {
+// FakeListener creates a custom listener that avoids opening a socket in order
+// to establish communication between an HTTP client and server
+type FakeListener struct {
 	closeOnce      sync.Once
 	channel        chan net.Conn
 	serverEndpoint io.Closer
@@ -42,11 +43,11 @@ type fakeListener struct {
 }
 
 // newFakeListener creates an instance of fakeListener. This will pass requests to the HTTP server as part of the testing harness.
-func newFakeListener() *fakeListener {
+func newFakeListener() *FakeListener {
 	s2c, c2s := net.Pipe()
 	c := make(chan net.Conn, 1)
 	c <- s2c
-	return &fakeListener{
+	return &FakeListener{
 		channel:        c,
 		serverEndpoint: s2c,
 		clientEndpoint: c2s,
@@ -55,7 +56,7 @@ func newFakeListener() *fakeListener {
 
 // Accept passes a network connection to the HTTP server to enable bidirectional communication with the client.
 // It will return an error if Accept is called after the listener was closed.
-func (l *fakeListener) Accept() (net.Conn, error) {
+func (l *FakeListener) Accept() (net.Conn, error) {
 	ch, ok := <-l.channel
 	if !ok {
 		return nil, errors.New("Listener closed")
@@ -64,7 +65,7 @@ func (l *fakeListener) Accept() (net.Conn, error) {
 }
 
 // Close will close the two network connections and the listener.
-func (l *fakeListener) Close() error {
+func (l *FakeListener) Close() error {
 	l.closeOnce.Do(func() {
 		close(l.channel)
 	})
@@ -77,29 +78,33 @@ func (l *fakeListener) Close() error {
 }
 
 // Addr returns the network address of the client endpoint.
-func (l *fakeListener) Addr() net.Addr {
+func (l *FakeListener) Addr() net.Addr {
 	return l.clientEndpoint.LocalAddr()
 }
 
 // SendRequest writes a request to the client endpoint connection. This will be passed to the server through the listener.
 // The function blocks until the server has finished reading the message.
-func (l *fakeListener) sendRequest(request []byte) error {
+func (l *FakeListener) sendRequest(request []byte) error {
 	n, err := l.clientEndpoint.Write(request)
+
+	if err != nil {
+		return err
+	}
 	if n != len(request) {
 		return errors.New("client connection failed to write the entire request")
 	}
-	return err
+	return nil
 }
 
 // readResponse reads the response from the clientEndpoint connection, sent by the listening server.
 // It will block until the server has sent a response.
-func (l *fakeListener) readResponse(bytes []byte) (int, error) {
+func (l *FakeListener) readResponse(bytes []byte) (int, error) {
 	return l.clientEndpoint.Read(bytes)
 }
 
 // MakeRequest instantiates a new http.Server, sends the request provided as argument and returns the response.
 // 'callback' will be called in the http.Handler with the http.Request that the handler receives.
-// The size of the response is hardcoded as 4096 bytes. If the response received is larger, an error will be returned.
+// The size of the response is limited to 4096 bytes. If the response received is larger, an error will be returned.
 func MakeRequest(ctx context.Context, req []byte, callback func(*http.Request)) ([]byte, error) {
 	listener := newFakeListener()
 	defer listener.Close()
@@ -115,12 +120,13 @@ func MakeRequest(ctx context.Context, req []byte, callback func(*http.Request)) 
 
 	resp := make([]byte, 4096)
 	n, err := listener.readResponse(resp)
-	if n == 4096 {
-		return nil, errors.New("response larger than or equal to 4096 bytes")
-	}
 	if err != nil {
 		return nil, err
 	}
+	if n == 4096 {
+		return nil, errors.New("response larger than or equal to 4096 bytes")
+	}
+	
 
 	return resp[:n], server.Shutdown(ctx)
 }
