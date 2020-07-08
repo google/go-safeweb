9115dac88510c83917dce49592871b743b83367c
Mattias Grenfeldt <grenfeldt@google.com>
2020-07-08 14:39:21 +0200

    Referer tests

---
+++ tests/requestparsing/referer_test.go
@@ -0,0 +1,71 @@
+package requestparsing
+
+import (
+	"bytes"
+	"context"
+	"net/http"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+
+	"../../testing/requesttesting"
+)
+
+type refererWant struct {
+	headers map[string][]string
+	referer string
+}
+
+var refererTests = []struct {
+	name    string
+	request []byte
+	want    refererWant
+}{
+	{
+		name: "Basic",
+		request: []byte("GET / HTTP/1.1\r\n" +
+			"Host: localhost:8080\r\n" +
+			"Referer: http://example.com\r\n" +
+			"\r\n"),
+		want: refererWant{
+			headers: map[string][]string{"Referer": []string{"http://example.com"}},
+			referer: "http://example.com",
+		},
+	},
+	{
+		name: "Ordering",
+		request: []byte("GET / HTTP/1.1\r\n" +
+			"Host: localhost:8080\r\n" +
+			"Referer: http://example.com\r\n" +
+			"Referer: http://evil.com\r\n" +
+			"\r\n"),
+		want: refererWant{
+			headers: map[string][]string{"Referer": []string{"http://example.com", "http://evil.com"}},
+			referer: "http://example.com",
+		},
+	},
+}
+
+func TestReferer(t *testing.T) {
+	for _, tt := range refererTests {
+		t.Run(tt.name, func(t *testing.T) {
+			resp, err := requesttesting.MakeRequest(context.Background(), tt.request, func(r *http.Request) {
+				if diff := cmp.Diff(tt.want.headers, map[string][]string(r.Header)); diff != "" {
+					t.Errorf("r.Header mismatch (-want +got):\n%s", diff)
+				}
+
+				if r.Referer() != tt.want.referer {
+					t.Errorf("r.Referer() got: %q want: %q", r.Referer(), tt.want.referer)
+				}
+			})
+			if err != nil {
+				t.Errorf("MakeRequest() got: %v want: nil", err)
+			}
+
+			if !bytes.HasPrefix(resp, []byte(statusOK)) {
+				got := string(resp[:bytes.IndexByte(resp, '\n')+1])
+				t.Errorf("status code got: %q want: %q", got, statusOK)
+			}
+		})
+	}
+}
