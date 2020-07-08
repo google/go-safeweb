b2b1d394e8fb84aaa740ace86a53cb71bfe07af8
Mattias Grenfeldt <grenfeldt@google.com>
2020-07-08 18:54:01 +0200

    Moved table testing variables into test functions

--- tests/requestparsing/basicauth_test.go
+++ tests/requestparsing/basicauth_test.go
@@ -11,98 +11,98 @@ import (
 	"github.com/google/go-safeweb/testing/requesttesting"
 )
 
-type basicAuthWant struct {
-	headers  map[string][]string
-	ok       bool
-	username string
-	password string
-}
-
-var basicAuthTests = []struct {
-	name    string
-	request []byte
-	want    basicAuthWant
-}{
-	{
-		name: "Basic",
-		request: []byte("GET / HTTP/1.1\r\n" +
-			"Host: localhost:8080\r\n" +
-			// Base64 encoding of "Pelle:Password".
-			"Authorization: Basic UGVsbGU6UGFzc3dvcmQ=\r\n" +
-			"\r\n"),
-		want: basicAuthWant{
-			// Same Base64 as above.
-			headers:  map[string][]string{"Authorization": []string{"Basic UGVsbGU6UGFzc3dvcmQ="}},
-			ok:       true,
-			username: "Pelle",
-			password: "Password",
-		},
-	},
-	{
-		name: "NoTrailingEquals",
-		request: []byte("GET / HTTP/1.1\r\n" +
-			"Host: localhost:8080\r\n" +
-			// Base64 encoding of "Pelle:Password" without trailing equals.
-			"Authorization: Basic UGVsbGU6UGFzc3dvcmQ\r\n" +
-			"\r\n"),
-		want: basicAuthWant{
-			// Same Base64 as above.
-			headers:  map[string][]string{"Authorization": []string{"Basic UGVsbGU6UGFzc3dvcmQ"}},
-			ok:       false,
-			username: "",
-			password: "",
-		},
-	},
-	{
-		name: "DoubleColon",
-		request: []byte("GET / HTTP/1.1\r\n" +
-			"Host: localhost:8080\r\n" +
-			// Base64 encoding of "Pelle:Password:Password".
-			"Authorization: Basic UGVsbGU6UGFzc3dvcmQ6UGFzc3dvcmQ=\r\n" +
-			"\r\n"),
-		want: basicAuthWant{
-			// Same Base64 as above.
-			headers:  map[string][]string{"Authorization": []string{"Basic UGVsbGU6UGFzc3dvcmQ6UGFzc3dvcmQ="}},
-			ok:       true,
-			username: "Pelle",
-			password: "Password:Password",
-		},
-	},
-	{
-		name: "NotBasic",
-		request: []byte("GET / HTTP/1.1\r\n" +
-			"Host: localhost:8080\r\n" +
-			// Base64 encoding of "Pelle:Password:Password".
-			"Authorization: xasic UGVsbGU6UGFzc3dvcmQ6UGFzc3dvcmQ=\r\n" +
-			"\r\n"),
-		want: basicAuthWant{
-			// Same Base64 as above.
-			headers:  map[string][]string{"Authorization": []string{"xasic UGVsbGU6UGFzc3dvcmQ6UGFzc3dvcmQ="}},
-			ok:       false,
-			username: "",
-			password: "",
-		},
-	},
-	{
-		name: "Ordering",
-		request: []byte("GET / HTTP/1.1\r\n" +
-			"Host: localhost:8080\r\n" +
-			// Base64 encoding of "AAA:aaa".
-			"Authorization: basic QUFBOmFhYQ==\r\n" +
-			// Base64 encoding of "BBB:bbb".
-			"Authorization: basic QkJCOmJiYg==\r\n" +
-			"\r\n"),
-		want: basicAuthWant{
-			// Base64 encoding of "AAA:aaa" and then of "BBB:bbb" in that order.
-			headers:  map[string][]string{"Authorization": []string{"basic QUFBOmFhYQ==", "basic QkJCOmJiYg=="}},
-			ok:       true,
-			username: "AAA",
-			password: "aaa",
-		},
-	},
-}
-
 func TestBasicAuth(t *testing.T) {
+	type basicAuthWant struct {
+		headers  map[string][]string
+		ok       bool
+		username string
+		password string
+	}
+
+	var basicAuthTests = []struct {
+		name    string
+		request []byte
+		want    basicAuthWant
+	}{
+		{
+			name: "Basic",
+			request: []byte("GET / HTTP/1.1\r\n" +
+				"Host: localhost:8080\r\n" +
+				// Base64 encoding of "Pelle:Password".
+				"Authorization: Basic UGVsbGU6UGFzc3dvcmQ=\r\n" +
+				"\r\n"),
+			want: basicAuthWant{
+				// Same Base64 as above.
+				headers:  map[string][]string{"Authorization": []string{"Basic UGVsbGU6UGFzc3dvcmQ="}},
+				ok:       true,
+				username: "Pelle",
+				password: "Password",
+			},
+		},
+		{
+			name: "NoTrailingEquals",
+			request: []byte("GET / HTTP/1.1\r\n" +
+				"Host: localhost:8080\r\n" +
+				// Base64 encoding of "Pelle:Password" without trailing equals.
+				"Authorization: Basic UGVsbGU6UGFzc3dvcmQ\r\n" +
+				"\r\n"),
+			want: basicAuthWant{
+				// Same Base64 as above.
+				headers:  map[string][]string{"Authorization": []string{"Basic UGVsbGU6UGFzc3dvcmQ"}},
+				ok:       false,
+				username: "",
+				password: "",
+			},
+		},
+		{
+			name: "DoubleColon",
+			request: []byte("GET / HTTP/1.1\r\n" +
+				"Host: localhost:8080\r\n" +
+				// Base64 encoding of "Pelle:Password:Password".
+				"Authorization: Basic UGVsbGU6UGFzc3dvcmQ6UGFzc3dvcmQ=\r\n" +
+				"\r\n"),
+			want: basicAuthWant{
+				// Same Base64 as above.
+				headers:  map[string][]string{"Authorization": []string{"Basic UGVsbGU6UGFzc3dvcmQ6UGFzc3dvcmQ="}},
+				ok:       true,
+				username: "Pelle",
+				password: "Password:Password",
+			},
+		},
+		{
+			name: "NotBasic",
+			request: []byte("GET / HTTP/1.1\r\n" +
+				"Host: localhost:8080\r\n" +
+				// Base64 encoding of "Pelle:Password:Password".
+				"Authorization: xasic UGVsbGU6UGFzc3dvcmQ6UGFzc3dvcmQ=\r\n" +
+				"\r\n"),
+			want: basicAuthWant{
+				// Same Base64 as above.
+				headers:  map[string][]string{"Authorization": []string{"xasic UGVsbGU6UGFzc3dvcmQ6UGFzc3dvcmQ="}},
+				ok:       false,
+				username: "",
+				password: "",
+			},
+		},
+		{
+			name: "Ordering",
+			request: []byte("GET / HTTP/1.1\r\n" +
+				"Host: localhost:8080\r\n" +
+				// Base64 encoding of "AAA:aaa".
+				"Authorization: basic QUFBOmFhYQ==\r\n" +
+				// Base64 encoding of "BBB:bbb".
+				"Authorization: basic QkJCOmJiYg==\r\n" +
+				"\r\n"),
+			want: basicAuthWant{
+				// Base64 encoding of "AAA:aaa" and then of "BBB:bbb" in that order.
+				headers:  map[string][]string{"Authorization": []string{"basic QUFBOmFhYQ==", "basic QkJCOmJiYg=="}},
+				ok:       true,
+				username: "AAA",
+				password: "aaa",
+			},
+		},
+	}
+
 	for _, tt := range basicAuthTests {
 		t.Run(tt.name, func(t *testing.T) {
 			resp, err := requesttesting.MakeRequest(context.Background(), tt.request, func(r *http.Request) {
--- tests/requestparsing/referer_test.go
+++ tests/requestparsing/referer_test.go
@@ -11,42 +11,42 @@ import (
 	"github.com/google/go-safeweb/testing/requesttesting"
 )
 
-type refererWant struct {
-	headers map[string][]string
-	referer string
-}
-
-var refererTests = []struct {
-	name    string
-	request []byte
-	want    refererWant
-}{
-	{
-		name: "Basic",
-		request: []byte("GET / HTTP/1.1\r\n" +
-			"Host: localhost:8080\r\n" +
-			"Referer: http://example.com\r\n" +
-			"\r\n"),
-		want: refererWant{
-			headers: map[string][]string{"Referer": []string{"http://example.com"}},
-			referer: "http://example.com",
-		},
-	},
-	{
-		name: "Ordering",
-		request: []byte("GET / HTTP/1.1\r\n" +
-			"Host: localhost:8080\r\n" +
-			"Referer: http://example.com\r\n" +
-			"Referer: http://evil.com\r\n" +
-			"\r\n"),
-		want: refererWant{
-			headers: map[string][]string{"Referer": []string{"http://example.com", "http://evil.com"}},
-			referer: "http://example.com",
-		},
-	},
-}
-
 func TestReferer(t *testing.T) {
+	type refererWant struct {
+		headers map[string][]string
+		referer string
+	}
+
+	var refererTests = []struct {
+		name    string
+		request []byte
+		want    refererWant
+	}{
+		{
+			name: "Basic",
+			request: []byte("GET / HTTP/1.1\r\n" +
+				"Host: localhost:8080\r\n" +
+				"Referer: http://example.com\r\n" +
+				"\r\n"),
+			want: refererWant{
+				headers: map[string][]string{"Referer": []string{"http://example.com"}},
+				referer: "http://example.com",
+			},
+		},
+		{
+			name: "Ordering",
+			request: []byte("GET / HTTP/1.1\r\n" +
+				"Host: localhost:8080\r\n" +
+				"Referer: http://example.com\r\n" +
+				"Referer: http://evil.com\r\n" +
+				"\r\n"),
+			want: refererWant{
+				headers: map[string][]string{"Referer": []string{"http://example.com", "http://evil.com"}},
+				referer: "http://example.com",
+			},
+		},
+	}
+
 	for _, tt := range refererTests {
 		t.Run(tt.name, func(t *testing.T) {
 			resp, err := requesttesting.MakeRequest(context.Background(), tt.request, func(r *http.Request) {
