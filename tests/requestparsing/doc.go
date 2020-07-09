41474148c8893e335be0ab0f76e5b5033c376169
Mattias Grenfeldt <grenfeldt@google.com>
2020-07-09 12:00:49 +0200

    Replaced README.md with doc.go

---
+++ tests/requestparsing/doc.go
@@ -0,0 +1,6 @@
+// Package requestparsing contains tests to verify the
+// request parsing behavior of `net/http` in Go's standard library.
+// Note: All tests use verbatim newline characters in their
+// requests instead of using multiline strings to ensure that
+// \r and \n end up in exactly the right places.
+package requestparsing
