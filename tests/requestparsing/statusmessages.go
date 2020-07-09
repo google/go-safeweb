a51b00483c66658faa449bd095639f04453cb1b6
Mattias Grenfeldt <grenfeldt@google.com>
2020-07-09 11:52:47 +0200

    Added extractStatus helper

--- tests/requestparsing/statusmessages.go
+++ tests/requestparsing/statusmessages.go
@@ -1,10 +1,20 @@
 package requestparsing
 
+import "bytes"
+
 const (
-	statusOK                 = "HTTP/1.1 200 OK\r\n"
-	statusTooManyHostHeaders = "HTTP/1.1 400 Bad Request: too many Host headers\r\n"
-	statusNotImplemented     = "HTTP/1.1 501 Not Implemented\r\n"
-	statusBadRequest         = "HTTP/1.1 400 Bad Request\r\n"
-	statusInvalidHeaderName  = "HTTP/1.1 400 Bad Request: invalid header name\r\n"
-	statusInvalidHeaderValue = "HTTP/1.1 400 Bad Request: invalid header value\r\n"
+	statusOK                 = "HTTP/1.1 200 OK"
+	statusTooManyHostHeaders = "HTTP/1.1 400 Bad Request: too many Host headers"
+	statusNotImplemented     = "HTTP/1.1 501 Not Implemented"
+	statusBadRequest         = "HTTP/1.1 400 Bad Request"
+	statusInvalidHeaderName  = "HTTP/1.1 400 Bad Request: invalid header name"
+	statusInvalidHeaderValue = "HTTP/1.1 400 Bad Request: invalid header value"
 )
+
+func extractStatus(response []byte) string {
+	position := bytes.IndexByte(response, '\r')
+	if position == -1 {
+		return ""
+	}
+	return string(response[:position])
+}
