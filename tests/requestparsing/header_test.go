f74587f2e30f677c0e6dc57962a6a10365210629
Mattias Grenfeldt <grenfeldt@google.com>
2020-07-09 11:52:47 +0200

    Added extractStatus helper

--- tests/requestparsing/header_test.go
+++ tests/requestparsing/header_test.go
@@ -1,7 +1,6 @@
 package requestparsing
 
 import (
-	"bytes"
 	"context"
 	"net/http"
 	"testing"
@@ -106,9 +105,8 @@ func TestHeaderParsing(t *testing.T) {
 				t.Fatalf("MakeRequest() got err: %v", err)
 			}
 
-			if !bytes.HasPrefix(resp, []byte(statusOK)) {
-				got := string(resp[:bytes.IndexByte(resp, '\n')+1])
-				t.Errorf("status code got: %q want: %q", got, statusOK)
+			if got, want := extractStatus(resp), statusOK; got != want {
+				t.Errorf("status code got: %q want: %q", got, want)
 			}
 		})
 	}
@@ -187,8 +185,7 @@ func TestStatusCode(t *testing.T) {
 				t.Fatalf("MakeRequest() got err: %v", err)
 			}
 
-			if !bytes.HasPrefix(resp, []byte(tt.want)) {
-				got := string(resp[:bytes.IndexByte(resp, '\n')+1])
+			if got := extractStatus(resp); got != tt.want {
 				t.Errorf("status code got: %q want: %q", got, tt.want)
 			}
 		})
--- tests/requestparsing/useragent_test.go
+++ tests/requestparsing/useragent_test.go
@@ -1,7 +1,6 @@
 package requestparsing
 
 import (
-	"bytes"
 	"context"
 	"net/http"
 	"testing"
@@ -85,9 +84,8 @@ func TestUserAgent(t *testing.T) {
 				t.Fatalf("MakeRequest() got err: %v", err)
 			}
 
-			if !bytes.HasPrefix(resp, []byte(statusOK)) {
-				got := string(resp[:bytes.IndexByte(resp, '\n')+1])
-				t.Errorf("status code got: %q want: %q", got, statusOK)
+			if got, want := extractStatus(resp), statusOK; got != want {
+				t.Errorf("status code got: %q want: %q", got, want)
 			}
 		})
 	}
