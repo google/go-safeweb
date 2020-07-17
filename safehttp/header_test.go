fd9e0e54c69df9894ecce773dad3afc3a5732440
Mattias Grenfeldt <grenfeldt@google.com>
2020-07-17 11:42:37 +0200

    Added extra tests

--- safehttp/header_test.go
+++ safehttp/header_test.go
@@ -43,6 +43,17 @@ func TestSetCanonicalization(t *testing.T) {
 
 func TestSetSetCookie(t *testing.T) {
 	h := newHeader(http.Header{})
+	cookie := &http.Cookie{Name: "x", Value: "y"}
+	h.SetCookie(cookie)
+	if err := h.Set("Set-Cookie", "x=y"); err == nil {
+		t.Error(`h.Set("Set-Cookie", "x=y") got: nil want: error`)
+	}
+	if diff := cmp.Diff([]string{"x=y"}, h.Values("Set-Cookie")); diff != "" {
+		t.Errorf(`h.Values("Set-Cookie") mismatch (-want +got):\n%s`, diff)
+	}
+}
+func TestSetEmptySetCookie(t *testing.T) {
+	h := newHeader(http.Header{})
 	if err := h.Set("Set-Cookie", "x=y"); err == nil {
 		t.Error(`h.Set("Set-Cookie", "x=y") got: nil want: error`)
 	}
@@ -52,6 +63,20 @@ func TestSetSetCookie(t *testing.T) {
 }
 
 func TestSetImmutable(t *testing.T) {
+	h := newHeader(http.Header{})
+	if err := h.Set("Foo-Key", "Pizza-Value"); err != nil {
+		t.Errorf(`h.Set("Foo-Key", "Pizza-Value") got: %v want: nil`, err)
+	}
+	h.MarkImmutable("Foo-Key")
+	if err := h.Set("Foo-Key", "Bar-Value"); err == nil {
+		t.Error(`h.Set("Foo-Key", "Bar-Value") got: nil want: error`)
+	}
+	if diff := cmp.Diff([]string{"Pizza-Value"}, h.Values("Foo-Key")); diff != "" {
+		t.Errorf(`h.Values("Foo-Key") mismatch (-want +got):\n%s`, diff)
+	}
+}
+
+func TestSetEmptyImmutable(t *testing.T) {
 	h := newHeader(http.Header{})
 	h.MarkImmutable("Foo-Key")
 	if err := h.Set("Foo-Key", "Bar-Value"); err == nil {
@@ -67,10 +92,10 @@ func TestAdd(t *testing.T) {
 	if err := h.Add("Foo-Key", "Bar-Value"); err != nil {
 		t.Fatalf(`h.Add("Foo-Key", "Bar-Value") got err: %v want: nil`, err)
 	}
-	if err := h.Add("Foo-Key", "Bar-Value-2"); err != nil {
-		t.Fatalf(`h.Add("Foo-Key", "Bar-Value-2") got err: %v want: nil`, err)
+	if err := h.Add("Foo-Key", "Pizza-Value"); err != nil {
+		t.Fatalf(`h.Add("Foo-Key", "Pizza-Value") got err: %v want: nil`, err)
 	}
-	if diff := cmp.Diff([]string{"Bar-Value", "Bar-Value-2"}, h.Values("Foo-Key")); diff != "" {
+	if diff := cmp.Diff([]string{"Bar-Value", "Pizza-Value"}, h.Values("Foo-Key")); diff != "" {
 		t.Errorf(`h.Values("Foo-Key") mismatch (-want +got):\n%s`, diff)
 	}
 }
@@ -80,15 +105,26 @@ func TestAddCanonicalization(t *testing.T) {
 	if err := h.Add("fOo-KeY", "Bar-Value"); err != nil {
 		t.Fatalf(`h.Add("fOo-KeY", "Bar-Value") got err: %v want: nil`, err)
 	}
-	if err := h.Add("FoO-kEy", "Bar-Value-2"); err != nil {
-		t.Fatalf(`h.Add("FoO-kEy", "Bar-Value-2") got err: %v want: nil`, err)
+	if err := h.Add("FoO-kEy", "Pizza-Value"); err != nil {
+		t.Fatalf(`h.Add("FoO-kEy", "Pizza-Value") got err: %v want: nil`, err)
 	}
-	if diff := cmp.Diff([]string{"Bar-Value", "Bar-Value-2"}, h.Values("fOO-KEY")); diff != "" {
+	if diff := cmp.Diff([]string{"Bar-Value", "Pizza-Value"}, h.Values("fOO-KEY")); diff != "" {
 		t.Errorf(`h.Values("fOO-KEY")) mismatch (-want +got):\n%s`, diff)
 	}
 }
 
 func TestAddSetCookie(t *testing.T) {
+	h := newHeader(http.Header{})
+	cookie := &http.Cookie{Name: "x", Value: "y"}
+	h.SetCookie(cookie)
+	if err := h.Add("Set-Cookie", "x=y"); err == nil {
+		t.Error(`h.Add("Set-Cookie", "x=y") got: nil want: error`)
+	}
+	if diff := cmp.Diff([]string{"x=y"}, h.Values("Set-Cookie")); diff != "" {
+		t.Errorf(`h.Values("Set-Cookie") mismatch (-want +got):\n%s`, diff)
+	}
+}
+func TestAddEmptySetCookie(t *testing.T) {
 	h := newHeader(http.Header{})
 	if err := h.Add("Set-Cookie", "x=y"); err == nil {
 		t.Error(`h.Add("Set-Cookie", "x=y") got: nil want: error`)
@@ -104,10 +140,21 @@ func TestAddImmutable(t *testing.T) {
 		t.Fatalf(`h.Add("Foo-Key", "Bar-Value") got err: %v want: nil`, err)
 	}
 	h.MarkImmutable("Foo-Key")
-	if err := h.Add("Foo-Key", "Bar-Value-2"); err == nil {
-		t.Error(`h.Add("Set-Cookie", "Bar-Value") got: nil want: error`)
+	if err := h.Add("Foo-Key", "Pizza-Value"); err == nil {
+		t.Error(`h.Add("Foo-Key", "Pizza-Value") got: nil want: error`)
 	}
 	if diff := cmp.Diff([]string{"Bar-Value"}, h.Values("Foo-Key")); diff != "" {
+		t.Errorf(`h.Values("Foo-Key") mismatch (-want +got):\n%s`, diff)
+	}
+}
+
+func TestAddEmptyImmutable(t *testing.T) {
+	h := newHeader(http.Header{})
+	h.MarkImmutable("Foo-Key")
+	if err := h.Add("Foo-Key", "Pizza-Value"); err == nil {
+		t.Error(`h.Add("Foo-Key", "Pizza-Value") got: nil want: error`)
+	}
+	if diff := cmp.Diff([]string{}, h.Values("Foo-Key")); diff != "" {
 		t.Errorf(`h.Values("Foo-Key") mismatch (-want +got):\n%s`, diff)
 	}
 }
@@ -140,8 +187,23 @@ func TestDelCanonicalization(t *testing.T) {
 
 func TestDelSetCookie(t *testing.T) {
 	h := newHeader(http.Header{})
+	cookie := &http.Cookie{Name: "x", Value: "y"}
+	h.SetCookie(cookie)
 	if err := h.Del("Set-Cookie"); err == nil {
 		t.Error(`h.Del("Set-Cookie") got: nil want: error`)
+	}
+	if diff := cmp.Diff([]string{"x=y"}, h.Values("Set-Cookie")); diff != "" {
+		t.Errorf(`h.Values("Set-Cookie") mismatch (-want +got):\n%s`, diff)
+	}
+}
+
+func TestDelEmptySetCookie(t *testing.T) {
+	h := newHeader(http.Header{})
+	if err := h.Del("Set-Cookie"); err == nil {
+		t.Error(`h.Del("Set-Cookie") got: nil want: error`)
+	}
+	if diff := cmp.Diff([]string{}, h.Values("Set-Cookie")); diff != "" {
+		t.Errorf(`h.Values("Set-Cookie") mismatch (-want +got):\n%s`, diff)
 	}
 }
 
@@ -152,9 +214,20 @@ func TestDelImmutable(t *testing.T) {
 	}
 	h.MarkImmutable("Foo-Key")
 	if err := h.Del("Foo-Key"); err == nil {
-		t.Error(`h.Add("Set-Cookie", "Bar-Value") got: nil want: error`)
+		t.Error(`h.Del("Foo-Key") got: nil want: error`)
 	}
 	if diff := cmp.Diff([]string{"Bar-Value"}, h.Values("Foo-Key")); diff != "" {
+		t.Errorf(`h.Values("Foo-Key") mismatch (-want +got):\n%s`, diff)
+	}
+}
+
+func TestDelEmptyImmutable(t *testing.T) {
+	h := newHeader(http.Header{})
+	h.MarkImmutable("Foo-Key")
+	if err := h.Del("Foo-Key"); err == nil {
+		t.Error(`h.Del("Foo-Key") got: nil want: error`)
+	}
+	if diff := cmp.Diff([]string{}, h.Values("Foo-Key")); diff != "" {
 		t.Errorf(`h.Values("Foo-Key") mismatch (-want +got):\n%s`, diff)
 	}
 }
@@ -190,5 +263,65 @@ func TestValuesModifyImmutable(t *testing.T) {
 	v[0] = "Evil-Value"
 	if got, want := h.Get("Foo-Key"), "Bar-Value"; got != want {
 		t.Errorf(`h.Get("Foo-Key") got: %v want: %v`, got, want)
+	}
+}
+
+// TestValuesOrdering ensures that the Values() function
+// return the headers values in the order that they were
+// set.
+func TestValuesOrdering(t *testing.T) {
+	t.Run("Bar Pizza", func(t *testing.T) {
+		h := newHeader(http.Header{})
+		if err := h.Add("Foo-Key", "Bar-Value"); err != nil {
+			t.Fatalf(`h.Add("Foo-Key", "Bar-Value") got err: %v want: nil`, err)
+		}
+		if err := h.Add("Foo-Key", "Pizza-Value"); err != nil {
+			t.Fatalf(`h.Add("Foo-Key", "Pizza-Value") got err: %v want: nil`, err)
+		}
+		if diff := cmp.Diff([]string{"Bar-Value", "Pizza-Value"}, h.Values("Foo-Key")); diff != "" {
+			t.Errorf(`h.Values("Foo-Key") mismatch (-want +got):\n%s`, diff)
+		}
+	})
+
+	t.Run("Pizza Bar", func(t *testing.T) {
+		h := newHeader(http.Header{})
+		if err := h.Add("Foo-Key", "Pizza-Value"); err != nil {
+			t.Fatalf(`h.Add("Foo-Key", "Pizza-Value") got err: %v want: nil`, err)
+		}
+		if err := h.Add("Foo-Key", "Bar-Value"); err != nil {
+			t.Fatalf(`h.Add("Foo-Key", "Bar-Value") got err: %v want: nil`, err)
+		}
+		if diff := cmp.Diff([]string{"Pizza-Value", "Bar-Value"}, h.Values("Foo-Key")); diff != "" {
+			t.Errorf(`h.Values("Foo-Key") mismatch (-want +got):\n%s`, diff)
+		}
+	})
+}
+
+func TestManyEqualKeyValuePairs(t *testing.T) {
+	h := newHeader(http.Header{})
+	if err := h.Add("Foo-Key", "Bar-Value"); err != nil {
+		t.Fatalf(`h.Add("Foo-Key", "Bar-Value") got err: %v want: nil`, err)
+	}
+	if err := h.Add("Foo-Key", "Bar-Value"); err != nil {
+		t.Fatalf(`h.Add("Foo-Key", "Bar-Value") got err: %v want: nil`, err)
+	}
+	if diff := cmp.Diff([]string{"Bar-Value", "Bar-Value"}, h.Values("Foo-Key")); diff != "" {
+		t.Errorf(`h.Values("Foo-Key") mismatch (-want +got):\n%s`, diff)
+	}
+}
+
+// TestAddSet tests that calling Set() after calling Add() will
+// correctly remove the previously added header before setting
+// the new one.
+func TestAddSet(t *testing.T) {
+	h := newHeader(http.Header{})
+	if err := h.Add("Foo-Key", "Bar-Value"); err != nil {
+		t.Fatalf(`h.Add("Foo-Key", "Bar-Value") got err: %v want: nil`, err)
+	}
+	if err := h.Set("Foo-Key", "Pizza-Value"); err != nil {
+		t.Fatalf(`h.Set("Foo-Key", "Pizza-Value") got err: %v want: nil`, err)
+	}
+	if diff := cmp.Diff([]string{"Pizza-Value"}, h.Values("Foo-Key")); diff != "" {
+		t.Errorf(`h.Values("Foo-Key") mismatch (-want +got):\n%s`, diff)
 	}
 }
