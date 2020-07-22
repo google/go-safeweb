90fddbbf78daf81bc37ee124018f6e42f2def70e
Mara Mihali <maramihali@google.com>
2020-07-22 09:57:49 +0300

    Removed function name from error messages

--- safehttp/form.go
+++ safehttp/form.go
@@ -35,21 +35,20 @@ type MultipartForm struct {
 
 // Int TODO
 func (f *Form) Int(paramName string, defaultValue int) int {
-	const fnInt = "Int"
 	if f.err != nil {
 		return defaultValue
 	}
 	if !f.parsed {
-		panic(fnInt + ": form has not been parsed")
+		panic("form has not been parsed")
 	}
 	vals, ok := f.values[paramName]
 	if !ok {
-		f.err = errors.New(fnInt + ": no value found for key " + paramName)
+		f.err = errors.New("no value found for key " + paramName)
 		return defaultValue
 	}
 	paramVal, err := strconv.Atoi(vals[0])
 	if err != nil {
-		f.err = errors.New(fnInt + ": " + err.Error())
+		f.err = err
 		return defaultValue
 	}
 	return paramVal
@@ -57,36 +56,34 @@ func (f *Form) Int(paramName string, defaultValue int) int {
 
 // Uint TODO
 func (f *Form) Uint(paramName string, defaultValue uint64) uint64 {
-	const fnUint = "Uint"
 	if f.err != nil {
 		return defaultValue
 	}
 	if !f.parsed {
-		panic(fnUint + ": form has not been parsed")
+		panic("form has not been parsed")
 	}
 	vals, ok := f.values[paramName]
 	if !ok {
-		f.err = errors.New(fnUint + ": no value found for key " + paramName)
+		f.err = errors.New("no value found for key " + paramName)
 		return defaultValue
 	}
 	paramVal, err := strconv.ParseUint(vals[0], 10, 0)
 	if err != nil {
-		f.err = errors.New(fnUint + ": " + err.Error())
+		f.err = err
 		return defaultValue
 	}
 	return paramVal
 }
 func (f *Form) String(paramName string, defaultValue string) string {
-	const fnString = "String"
 	if f.err != nil {
 		return defaultValue
 	}
 	if !f.parsed {
-		panic(fnString + ": form has not been parsed")
+		panic("form has not been parsed")
 	}
 	vals, ok := f.values[paramName]
 	if !ok {
-		f.err = errors.New(fnString + ": no value found for key " + paramName)
+		f.err = errors.New("no value found for key " + paramName)
 		return defaultValue
 	}
 	return vals[0]
@@ -94,21 +91,20 @@ func (f *Form) String(paramName string, defaultValue string) string {
 
 // Float64 TODO
 func (f *Form) Float64(paramName string, defaultValue float64) float64 {
-	const fnFloat64 = "Float64"
 	if f.err != nil {
 		return defaultValue
 	}
 	if !f.parsed {
-		panic(fnFloat64 + ": form has not been parsed")
+		panic("form has not been parsed")
 	}
 	vals, ok := f.values[paramName]
 	if !ok {
-		f.err = errors.New(fnFloat64 + "no value found for key " + paramName)
+		f.err = errors.New("no value found for key " + paramName)
 		return defaultValue
 	}
 	paramVal, err := strconv.ParseFloat(vals[0], 64)
 	if err != nil {
-		f.err = errors.New(fnFloat64 + ": " + err.Error())
+		f.err = err
 		return defaultValue
 	}
 	return paramVal
@@ -116,21 +112,20 @@ func (f *Form) Float64(paramName string, defaultValue float64) float64 {
 
 // Bool TODO
 func (f *Form) Bool(paramName string, defaultValue bool) bool {
-	const fnBool = "Bool"
 	if f.err != nil {
 		return defaultValue
 	}
 	if !f.parsed {
-		panic(fnBool + ": form has not been parsed")
+		panic("form has not been parsed")
 	}
 	vals, ok := f.values[paramName]
 	if !ok {
-		f.err = errors.New(fnBool + ": no value found for key " + paramName)
+		f.err = errors.New("no value found for key " + paramName)
 		return defaultValue
 	}
 	if vals[0] != "true" {
 		if vals[0] != "false" {
-			f.err = errors.New(fnBool + ": values of form parameter " + paramName + " not a boolean")
+			f.err = errors.New("values of form parameter " + paramName + " not a boolean")
 		}
 		return false
 	}
@@ -139,17 +134,16 @@ func (f *Form) Bool(paramName string, defaultValue bool) bool {
 
 // Slice TODO
 func (f *Form) Slice(slice interface{}, paramName string) {
-	const fnSlice = "Slice"
 	if f.err != nil {
 		slice = nil
 		return
 	}
 	if !f.parsed {
-		panic(fnSlice + ": form has not been parsed")
+		panic("form has not been parsed")
 	}
 	vals, ok := f.values[paramName]
 	if !ok {
-		f.err = errors.New(fnSlice + ": no value found for key " + paramName)
+		f.err = errors.New("no value found for key " + paramName)
 		slice = nil
 	}
 	len := len(vals)
@@ -166,7 +160,7 @@ func (f *Form) Slice(slice interface{}, paramName string) {
 		for _, x := range vals {
 			x, err := strconv.Atoi(x)
 			if err != nil {
-				f.err = errors.New(fnSlice + ": " + err.Error())
+				f.err = err
 				slice = nil
 				return
 			}
@@ -178,7 +172,7 @@ func (f *Form) Slice(slice interface{}, paramName string) {
 		for _, x := range vals {
 			x, err := strconv.ParseUint(x, 10, 0)
 			if err != nil {
-				f.err = errors.New(fnSlice + ": " + err.Error())
+				f.err = err
 				slice = nil
 				return
 			}
@@ -190,7 +184,7 @@ func (f *Form) Slice(slice interface{}, paramName string) {
 		for _, x := range vals {
 			x, err := strconv.ParseFloat(x, 64)
 			if err != nil {
-				f.err = errors.New(fnSlice + ": " + err.Error())
+				f.err = err
 				slice = nil
 				return
 			}
@@ -202,7 +196,7 @@ func (f *Form) Slice(slice interface{}, paramName string) {
 		for _, x := range vals {
 			if x != "true" {
 				if x != "false" {
-					f.err = errors.New(fnSlice + ": " + ": values of form parameter " + paramName + " not a boolean")
+					f.err = errors.New(": values of form parameter " + paramName + " not a boolean")
 					slice = nil
 					return
 				}
@@ -212,7 +206,7 @@ func (f *Form) Slice(slice interface{}, paramName string) {
 			res = append(res, true)
 		}
 	default:
-		f.err = errors.New(fnSlice + ": slice type not supported")
+		f.err = errors.New("slice type not supported")
 		slice = nil
 	}
 
