5d48d4825e707003d08c72ac55acff1ce26f048b
empijei <robclap8@gmail.com>
2020-08-11 17:03:56 +0200

    plugins/htmlinject: Implement rewrite logic.

--- safehttp/plugins/htmlinject/htmlinject.go
+++ safehttp/plugins/htmlinject/htmlinject.go
@@ -1,3 +1,4 @@
+// Package htmlinject provides utilities to pre-process HTML templates and inject additional parts to them before parsing.
 package htmlinject
 
 import (
@@ -35,7 +36,7 @@ type Config []Rule
 
 // CSPNoncesDefault is the default config for CSP Nonces. The rewritten template
 // expects the CSPNonce Func to be available in the template to provide nonces.
-var CSPNoncesDefault = CSPNonces(`nonce="{{CSPNonce}}`)
+var CSPNoncesDefault = CSPNonces(`nonce="{{CSPNonce}}"`)
 
 // CSPNonces constructs a Config to add CSP nonces to a template. The given nonce
 // attribute will be automatically prefixed with the required empty space.
@@ -59,19 +60,26 @@ func CSPNonces(nonceAttr string) Config {
 // XSRFTokensDefault is the default config to add hidden inputs to forms to provide
 // an anti-XSRF token. The rewritten template expects the XSRFToken Func to be available
 // in the template to provide tokens and sets the name for the sent value to be "xsrftoken".
-var XSRFTokensDefault = XSRFTokens(`<input type="hidden" name="xsrftoken" value="{{XSRFToken}}">`)
+var XSRFTokensDefault = XSRFTokens(`<input type="hidden" name="xsrf-token" value="{{XSRFToken}}">`)
 
 // XSRFTokens constructs a Config to add the given string as a child node to forms.
 func XSRFTokens(inputTag string) Config {
-	return Config{Rule{OnTag: "form", AddNodes: []string{inputTag}}}
+	return Config{Rule{
+		Name:     "XSRFTokens on forms",
+		OnTag:    "form",
+		AddNodes: []string{inputTag}}}
 }
 
 // Transform rewrites the given template according to the given configs.
+// If the passed io.Rewriter has a `Size() int64` method it will be used to pre-allocate buffers.
 func Transform(src io.Reader, cfg ...Config) (tpl string, _ error) {
 	rw := rewriter{
 		rules:     map[string][]Rule{},
 		tokenizer: html.NewTokenizer(src),
 		out:       &strings.Builder{},
+	}
+	if sizer, ok := src.(interface{ Size() int64 }); ok {
+		rw.out.Grow(int(sizer.Size()))
 	}
 	for _, c := range cfg {
 		for _, r := range c {
@@ -94,12 +102,6 @@ type rewriter struct {
 // emitRaw copies the current raw token to the output.
 func (r rewriter) emitRaw() error {
 	_, err := r.out.Write(r.tokenizer.Raw())
-	return err
-}
-
-// emit writes the given slice to the output.
-func (r rewriter) emit(p []byte) error {
-	_, err := r.out.Write(p)
 	return err
 }
 
@@ -126,7 +128,66 @@ func (r rewriter) rewrite() error {
 }
 
 func (r rewriter) processTag() error {
-	// TODO(empijei): implement
-	return r.emitRaw()
+	// Copy raw tokens to better formats
+	var (
+		tagname    string
+		attributes = map[string]string{}
+		raw        = make([]byte, len(r.tokenizer.Raw()))
+	)
+	{
+		copy(raw, r.tokenizer.Raw())
+		tag, hasAttr := r.tokenizer.TagName()
+		tagname = string(tag)
+		for hasAttr {
+			key, val, more := r.tokenizer.TagAttr()
+			hasAttr = more
+			attributes[string(key)] = string(val)
+		}
+	}
 
+	// Filter rules by attributes
+	var triggeredRules []Rule
+	{
+		rules := r.rules[tagname]
+		for _, r := range rules {
+			match := true
+			for k, v := range r.WithAttributes {
+				if attributes[k] != v {
+					match = false
+				}
+			}
+			if match {
+				triggeredRules = append(triggeredRules, r)
+			}
+		}
+	}
+
+	// Emit the rewritten HTML
+	{
+		// Write the "<" symbol and the tag name, e.g. "<script"
+		if _, err := r.out.Write(raw[:len(tagname)+1]); err != nil {
+			return fmt.Errorf("copying beginning of tag: %w", err)
+		}
+		// Write the attributes we have to add
+		for _, rule := range triggeredRules {
+			for _, attr := range rule.AddAttributes {
+				if _, err := r.out.WriteString(attr); err != nil {
+					return fmt.Errorf("executing rule %q: %w", rule.Name, err)
+				}
+			}
+		}
+		// Write the rest of the opening tag, e.g. ` src="foo.js">`
+		if _, err := r.out.Write(raw[len(tagname)+1:]); err != nil {
+			return fmt.Errorf("copying end of tag: %w", err)
+		}
+		// Write the nodes we have to add
+		for _, rule := range triggeredRules {
+			for _, node := range rule.AddNodes {
+				if _, err := r.out.WriteString(node); err != nil {
+					return fmt.Errorf("executing rule %q: %w", rule.Name, err)
+				}
+			}
+		}
+	}
+	return nil
 }
--- safehttp/plugins/htmlinject/htmlinject_test.go
+++ safehttp/plugins/htmlinject/htmlinject_test.go
@@ -1,12 +1,107 @@
 package htmlinject
 
 import (
+	"html/template"
+	"os"
 	"strings"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
 )
 
+func ExampleTransform() {
+	const in = `
+<html>
+<head>
+<link rel=preload as="script" src="gopher.js">
+</head>
+<body>
+{{.Content}}
+<script type="application/javascript">alert("script")</script>
+<form>
+First name:<br>
+<input type="text" name="firstname"><br>
+Last name:<br>
+<input type="text" name="lastname">
+</form>
+</body>
+</html>
+`
+	got, err := Transform(strings.NewReader(in), CSPNoncesDefault, XSRFTokensDefault)
+	if err != nil {
+		// handle error
+		panic(err)
+	}
+	template.Must(template.New("example transform").Funcs(map[string]interface{}{
+		"XSRFToken": func() string { return "XSRFToken-secret" },
+		"CSPNonce":  func() string { return "CSPNonce-secret" },
+	}).Parse(got)).Execute(os.Stdout, map[string]string{"Content": "This is some content"})
+	// Output:
+	// <html>
+	// <head>
+	// <link nonce="CSPNonce-secret" rel=preload as="script" src="gopher.js">
+	// </head>
+	// <body>
+	// This is some content
+	// <script nonce="CSPNonce-secret" type="application/javascript">alert("script")</script>
+	// <form><input type="hidden" name="xsrf-token" value="XSRFToken-secret">
+	// First name:<br>
+	// <input type="text" name="firstname"><br>
+	// Last name:<br>
+	// <input type="text" name="lastname">
+	// </form>
+	// </body>
+	// </html>
+}
+
+func BenchmarkTransform(b *testing.B) {
+	b.ReportAllocs()
+	var (
+		config = []Config{CSPNoncesDefault, XSRFTokensDefault}
+		in     = `
+<html>
+<head>
+<link rel=preload as="script" src="gopher.js">
+</head>
+<body>
+<script type="application/javascript">alert("script")</script>
+<form>
+  First name:<br>
+  <input type="text" name="firstname"><br>
+  Last name:<br>
+  <input type="text" name="lastname">
+</form>
+</body>
+</html>
+`
+		want = `
+<html>
+<head>
+<link nonce="{{CSPNonce}}" rel=preload as="script" src="gopher.js">
+</head>
+<body>
+<script nonce="{{CSPNonce}}" type="application/javascript">alert("script")</script>
+<form><input type="hidden" name="xsrf-token" value="{{XSRFToken}}">
+  First name:<br>
+  <input type="text" name="firstname"><br>
+  Last name:<br>
+  <input type="text" name="lastname">
+</form>
+</body>
+</html>
+`
+	)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		got, err := Transform(strings.NewReader(in), config...)
+		if err != nil {
+			b.Fatalf("Transform: got err %q, didn't want one", err)
+		}
+		if got != want {
+			b.Errorf("got %q, want %q", got, want)
+		}
+	}
+}
 func TestTransform(t *testing.T) {
 	var tests = []struct {
 		name     string
@@ -48,10 +143,10 @@ Hello world
 			want: `
 <html>
 <head>
-<link nonce="{{.CSPNonce}}" rel=preload as="script" src="gopher.js">
+<link nonce="{{CSPNonce}}" rel=preload as="script" src="gopher.js">
 </head>
 <body>
-<script nonce="{{CSPNonce)}}" type="application/javascript">alert("script")</script>
+<script nonce="{{CSPNonce}}" type="application/javascript">alert("script")</script>
 </body>
 </html>
 `,
@@ -68,7 +163,7 @@ Hello world
 </form>
 `,
 			want: `
-<form><input type="hidden" name="xsrftoken" value="{{XSRFToken}}">
+<form><input type="hidden" name="xsrf-token" value="{{XSRFToken}}">
   First name:<br>
   <input type="text" name="firstname"><br>
   Last name:<br>
@@ -98,11 +193,11 @@ Hello world
 			want: `
 <html>
 <head>
-<link nonce="{{.CSPNonce}}" rel=preload as="script" src="gopher.js">
+<link nonce="{{CSPNonce}}" rel=preload as="script" src="gopher.js">
 </head>
 <body>
-<script nonce="{{.CSPNonce}}" type="application/javascript">alert("script")</script>
-<form><input type="hidden" name="xsrftoken" value="{{.XSRFToken}}">
+<script nonce="{{CSPNonce}}" type="application/javascript">alert("script")</script>
+<form><input type="hidden" name="xsrf-token" value="{{XSRFToken}}">
   First name:<br>
   <input type="text" name="firstname"><br>
   Last name:<br>
