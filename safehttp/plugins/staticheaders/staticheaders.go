b4e23bd987ab601ffb5dfebe6cba3be85e20db22
Mattias Grenfeldt <mattiasgrenfeldt@gmail.com>
2020-08-14 15:05:14 +0200

    Added a staticheader plugin
    It currently sets the X-Content-Type-Options header and the X-XSS-Protection header. There are no options to disable settings the headers.

---
+++ safehttp/plugins/staticheaders/staticheaders.go
@@ -0,0 +1,42 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package staticheaders
+
+import (
+	"github.com/google/go-safeweb/safehttp"
+)
+
+// Plugin claims and sets static headers on outgoing responses. The zero value
+// is ready to use.
+type Plugin struct{}
+
+// Before claims and sets the following headers:
+//  - X-Content-Type-Options: nosniff
+//  - X-XSS-Protection: 0
+func (Plugin) Before(w safehttp.ResponseWriter, r *safehttp.IncomingRequest) safehttp.Result {
+	h := w.Header()
+	setXCTO, err := h.Claim("X-Content-Type-Options")
+	if err != nil {
+		return w.ServerError(safehttp.StatusInternalServerError)
+	}
+	setXCTO([]string{"nosniff"})
+
+	setXXP, err := h.Claim("X-XSS-Protection")
+	if err != nil {
+		return w.ServerError(safehttp.StatusInternalServerError)
+	}
+	setXXP([]string{"0"})
+	return safehttp.Result{}
+}
---
+++ safehttp/plugins/staticheaders/staticheaders_test.go
@@ -0,0 +1,81 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package staticheaders_test
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-safeweb/safehttp"
+	"github.com/google/go-safeweb/safehttp/plugins/staticheaders"
+	"github.com/google/go-safeweb/safehttp/safehttptest"
+)
+
+func TestAlreadyClaimed(t *testing.T) {
+	alreadyClaimed := []string{"X-Content-Type-Options", "X-XSS-Protection"}
+
+	for _, h := range alreadyClaimed {
+		t.Run(h, func(t *testing.T) {
+			req := safehttptest.NewRequest(safehttp.MethodGet, "/", nil)
+			rr := safehttptest.NewResponseRecorder()
+			if _, err := rr.ResponseWriter.Header().Claim(h); err != nil {
+				t.Fatalf("rr.ResponseWriter.Header().Claim(h) got: %v want: nil", err)
+			}
+
+			p := staticheaders.Plugin{}
+			p.Before(rr.ResponseWriter, req)
+
+			if got, want := rr.Status(), 500; got != want {
+				t.Errorf("rr.Status() got: %v want: %v", got, want)
+			}
+
+			wantHeaders := map[string][]string{
+				"Content-Type":           {"text/plain; charset=utf-8"},
+				"X-Content-Type-Options": {"nosniff"},
+			}
+			if diff := cmp.Diff(wantHeaders, map[string][]string(rr.Header())); diff != "" {
+				t.Errorf("rr.Header() mismatch (-want +got):\n%s", diff)
+			}
+
+			if got, want := rr.Body(), "Internal Server Error\n"; got != want {
+				t.Errorf("rr.Body() got: %q want: %q", got, want)
+			}
+		})
+	}
+}
+
+func TestWorkingAsIntended(t *testing.T) {
+	req := safehttptest.NewRequest(safehttp.MethodGet, "/", nil)
+	rr := safehttptest.NewResponseRecorder()
+
+	p := staticheaders.Plugin{}
+	p.Before(rr.ResponseWriter, req)
+
+	if got, want := rr.Status(), 200; got != want {
+		t.Errorf("rr.Status() got: %v want: %v", got, want)
+	}
+
+	wantHeaders := map[string][]string{
+		"X-Content-Type-Options": {"nosniff"},
+		"X-Xss-Protection":       {"0"},
+	}
+	if diff := cmp.Diff(wantHeaders, map[string][]string(rr.Header())); diff != "" {
+		t.Errorf("rr.Header() mismatch (-want +got):\n%s", diff)
+	}
+
+	if got, want := rr.Body(), ""; got != want {
+		t.Errorf("rr.Body() got: %q want: %q", got, want)
+	}
+}
