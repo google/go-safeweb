67e5526ce4ff1cd89d887afbfbbdff03f35d48cf
Mattias Grenfeldt <grenfeldt@google.com>
2020-08-17 16:43:47 +0200

    Added first version of CSP plugin.
    It supports creating custom CSP policies. The interceptor contains two policies, one for enforcement and one for reporting. A default policy based on https://csp.withgoogle.com/docs/strict-csp.html is also provided.

---
+++ safehttp/plugins/csp/csp.go
@@ -0,0 +1,182 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package csp
+
+import (
+	"context"
+	"crypto/rand"
+	"encoding/base64"
+	"strings"
+
+	"github.com/google/go-safeweb/safehttp"
+)
+
+// Directive is the name of a single CSP directive.
+type Directive string
+
+const (
+	DirectiveScriptSrc Directive = "script-src"
+	DirectiveStyleSrc  Directive = "style-src"
+	DirectiveObjectSrc Directive = "object-src"
+	DirectiveBaseURI   Directive = "base-uri"
+	DirectiveReportURI Directive = "report-uri"
+)
+
+const (
+	ValueHTTPS         = "https:"
+	ValueHTTP          = "http:"
+	ValueUnsafeEval    = "'unsafe-eval'"
+	ValueUnsafeInline  = "'unsafe-inline'"
+	ValueNone          = "'none'"
+	ValueStrictDynamic = "'strict-dynamic'"
+)
+
+// PolicyDirective contains a single CSP directive.
+type PolicyDirective struct {
+	dir       Directive
+	vals      []string
+	nonces    bool
+	lastNonce string
+	// readRand is used for dependency injection in tests.
+	readRand func([]byte) (int, error)
+}
+
+// NewPolicyDirective creates a new PolicyDirective given the directive, the
+// values and whether or not a nonce directive should be added.
+func NewPolicyDirective(d Directive, values []string, nonces bool) *PolicyDirective {
+	return &PolicyDirective{
+		dir:      d,
+		vals:     values,
+		nonces:   nonces,
+		readRand: rand.Read,
+	}
+}
+
+// nonceSize is the size of the nonces in bytes.
+const nonceSize = 8
+
+func (pd *PolicyDirective) generateNonce() {
+	b := make([]byte, nonceSize)
+	_, err := pd.readRand(b)
+	if err != nil {
+		// TODO: handle this better, what should happen here?
+		panic(err)
+	}
+	pd.lastNonce = base64.StdEncoding.EncodeToString(b)
+}
+
+// Policy defines a CSP policy, containing many directives.
+type Policy struct {
+	Directives []*PolicyDirective
+}
+
+// NewPolicy creates a new strict, nonce-based CSP.
+// See https://csp.withgoogle.com/docs/strict-csp.html for more info.
+//
+// TODO: maybe reportURI should be safehttp.URL?
+func NewPolicy(reportURI string) *Policy {
+	return &Policy{
+		Directives: []*PolicyDirective{
+			NewPolicyDirective(DirectiveObjectSrc, []string{ValueNone}, false),
+			NewPolicyDirective(DirectiveScriptSrc, []string{
+				ValueUnsafeInline,
+				ValueUnsafeEval,
+				ValueStrictDynamic,
+				ValueHTTPS,
+				ValueHTTP,
+			}, true),
+			NewPolicyDirective(DirectiveBaseURI, []string{ValueNone}, false),
+			NewPolicyDirective(DirectiveReportURI, []string{reportURI}, false),
+		},
+	}
+}
+
+// Serialize serializes this policy for use in a Content-Security-Policy header
+// or in a Content-Security-Policy-Report-Only header. The nonces generated for
+// each directive is also returned.
+func (p Policy) Serialize() (csp string, nonces map[Directive]string) {
+	nonces = make(map[Directive]string)
+	b := strings.Builder{}
+
+	for i, d := range p.Directives {
+		if i != 0 {
+			b.WriteString("; ")
+		}
+		b.WriteString(string(d.dir))
+
+		if d.nonces {
+			d.generateNonce()
+			b.WriteString(" 'nonce-")
+			b.WriteString(d.lastNonce)
+			b.WriteString("'")
+			nonces[d.dir] = d.lastNonce
+		}
+
+		for _, v := range d.vals {
+			b.WriteString(" ")
+			b.WriteString(v)
+		}
+	}
+	return b.String(), nonces
+}
+
+// Interceptor intercepts requests and applies CSP policies.
+type Interceptor struct {
+	// EnforcementPolicy will be applied as the Content-Security-Policy header.
+	EnforcementPolicy *Policy
+
+	// ReportOnlyPolicy will be applied as the
+	// Content-Security-Policy-Report-Only header.
+	ReportOnlyPolicy *Policy
+}
+
+// Default creates a new CSP interceptor with a strict nonce-based policy in
+// enforcement mode.
+func Default(reportURI string) Interceptor {
+	return Interceptor{EnforcementPolicy: NewPolicy(reportURI)}
+}
+
+type ctxKey string
+
+// Before claims and sets the Content-Security-Policy header and the
+// Content-Security-Policy-Report-Only header.
+func (it Interceptor) Before(w safehttp.ResponseWriter, r *safehttp.IncomingRequest) safehttp.Result {
+	h := w.Header()
+	setCSP, err := h.Claim("Content-Security-Policy")
+	if err != nil {
+		return w.ServerError(safehttp.StatusInternalServerError)
+	}
+	if it.EnforcementPolicy != nil {
+		s, nonces := it.EnforcementPolicy.Serialize()
+		setCSP([]string{s})
+		if len(nonces) != 0 {
+			r.SetContext(context.WithValue(r.Context(), ctxKey("enforce"), nonces))
+		}
+	}
+
+	setCSPReportOnly, err := h.Claim("Content-Security-Policy-Report-Only")
+	if err != nil {
+		return w.ServerError(safehttp.StatusInternalServerError)
+	}
+	if it.ReportOnlyPolicy != nil {
+		s, nonces := it.ReportOnlyPolicy.Serialize()
+		setCSPReportOnly([]string{s})
+		if len(nonces) != 0 {
+			r.SetContext(context.WithValue(r.Context(), ctxKey("report"), nonces))
+		}
+	}
+
+	return safehttp.Result{}
+}
---
+++ safehttp/plugins/csp/csp_test.go
@@ -0,0 +1,251 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package csp
+
+import (
+	"testing"
+
+	"github.com/google/go-safeweb/safehttp"
+	"github.com/google/go-safeweb/safehttp/safehttptest"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func readRand(b []byte) (int, error) {
+	for i := range b {
+		b[i] = byte(i)
+	}
+	return len(b), nil
+}
+
+func (p *Policy) insertTestRandom(f func([]byte) (int, error)) {
+	if p == nil {
+		return
+	}
+	for i := range p.Directives {
+		p.Directives[i].readRand = f
+	}
+}
+
+func TestSerialize(t *testing.T) {
+	tests := []struct {
+		name       string
+		policy     *Policy
+		wantString string
+		wantNonces map[Directive]string
+	}{
+		{
+			name:       "Empty",
+			policy:     &Policy{},
+			wantString: "",
+			wantNonces: map[Directive]string{},
+		},
+		{
+			name: "Default",
+			policy: func() *Policy {
+				p := NewPolicy("https://foo.com/collector")
+				p.insertTestRandom(readRand)
+				return p
+			}(),
+			wantString: "object-src 'none'; script-src 'nonce-AAECAwQFBgc=' 'unsafe-inline' 'unsafe-eval' 'strict-dynamic' https: http:; base-uri 'none'; report-uri https://foo.com/collector",
+			wantNonces: map[Directive]string{DirectiveScriptSrc: "AAECAwQFBgc="},
+		},
+		{
+			name: "Empty Directive",
+			policy: &Policy{
+				Directives: []*PolicyDirective{
+					NewPolicyDirective(DirectiveScriptSrc, nil, false),
+				},
+			},
+			wantString: "script-src",
+			wantNonces: map[Directive]string{},
+		},
+		{
+			name: "No nonces",
+			policy: &Policy{
+				Directives: []*PolicyDirective{
+					NewPolicyDirective(DirectiveScriptSrc, []string{ValueUnsafeEval}, false),
+				},
+			},
+			wantString: "script-src 'unsafe-eval'",
+			wantNonces: map[Directive]string{},
+		},
+		{
+			name: "Two nonces",
+			policy: func() *Policy {
+				p := &Policy{
+					Directives: []*PolicyDirective{
+						NewPolicyDirective(DirectiveScriptSrc, []string{ValueUnsafeEval}, true),
+						NewPolicyDirective(DirectiveStyleSrc, []string{ValueUnsafeEval}, true),
+					},
+				}
+				p.insertTestRandom(readRand)
+				return p
+			}(),
+			wantString: "script-src 'nonce-AAECAwQFBgc=' 'unsafe-eval'; style-src 'nonce-AAECAwQFBgc=' 'unsafe-eval'",
+			wantNonces: map[Directive]string{
+				DirectiveScriptSrc: "AAECAwQFBgc=",
+				DirectiveStyleSrc:  "AAECAwQFBgc=",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s, nonces := tt.policy.Serialize()
+
+			if got := s; got != tt.wantString {
+				t.Errorf("tt.policy.Serialize() got: %q want: %q", got, tt.wantString)
+			}
+
+			if diff := cmp.Diff(tt.wantNonces, nonces); diff != "" {
+				t.Errorf("nonces mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestBefore(t *testing.T) {
+	tests := []struct {
+		name                  string
+		interceptor           Interceptor
+		wantEnforcementPolicy []string
+		wantReportPolicy      []string
+		wantEnforcementNonces map[Directive]string
+		wantReportNonces      map[Directive]string
+	}{
+		{
+			name:        "No policies",
+			interceptor: Interceptor{},
+		},
+		{
+			name: "Default policy",
+			interceptor: func() Interceptor {
+				p := Default("https://foo.com/collector")
+				p.EnforcementPolicy.insertTestRandom(readRand)
+				p.ReportOnlyPolicy.insertTestRandom(readRand)
+				return p
+			}(),
+			wantEnforcementPolicy: []string{"object-src 'none'; script-src 'nonce-AAECAwQFBgc=' 'unsafe-inline' 'unsafe-eval' 'strict-dynamic' https: http:; base-uri 'none'; report-uri https://foo.com/collector"},
+			wantEnforcementNonces: map[Directive]string{DirectiveScriptSrc: "AAECAwQFBgc="},
+		},
+		{
+			name: "Report",
+			interceptor: Interceptor{
+				ReportOnlyPolicy: &Policy{
+					Directives: []*PolicyDirective{
+						NewPolicyDirective(DirectiveScriptSrc, []string{ValueUnsafeInline}, false),
+					},
+				},
+			},
+			wantReportPolicy: []string{"script-src 'unsafe-inline'"},
+		},
+		{
+			name: "Report with nonces",
+			interceptor: Interceptor{
+				ReportOnlyPolicy: func() *Policy {
+					p := NewPolicy("https://foo.com/collector")
+					p.insertTestRandom(readRand)
+					return p
+				}(),
+			},
+			wantReportPolicy: []string{"object-src 'none'; script-src 'nonce-AAECAwQFBgc=' 'unsafe-inline' 'unsafe-eval' 'strict-dynamic' https: http:; base-uri 'none'; report-uri https://foo.com/collector"},
+			wantReportNonces: map[Directive]string{DirectiveScriptSrc: "AAECAwQFBgc="},
+		},
+		{
+			name: "Report and enforce",
+			interceptor: Interceptor{
+				ReportOnlyPolicy: &Policy{
+					Directives: []*PolicyDirective{
+						NewPolicyDirective(DirectiveScriptSrc, []string{ValueUnsafeInline}, false),
+						NewPolicyDirective(DirectiveBaseURI, []string{ValueNone}, false),
+					},
+				},
+				EnforcementPolicy: &Policy{
+					Directives: []*PolicyDirective{
+						NewPolicyDirective(DirectiveScriptSrc, []string{ValueUnsafeInline}, false),
+					},
+				},
+			},
+			wantEnforcementPolicy: []string{"script-src 'unsafe-inline'"},
+			wantReportPolicy:      []string{"script-src 'unsafe-inline'; base-uri 'none'"},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			rr := safehttptest.NewResponseRecorder()
+			req := safehttptest.NewRequest(safehttp.MethodGet, "/", nil)
+
+			tt.interceptor.Before(rr.ResponseWriter, req)
+
+			h := rr.Header()
+			if diff := cmp.Diff(tt.wantEnforcementPolicy, h.Values("Content-Security-Policy")); diff != "" {
+				t.Errorf("h.Values(\"Content-Security-Policy\") mismatch (-want +got):\n%s", diff)
+			}
+
+			if diff := cmp.Diff(tt.wantReportPolicy, h.Values("Content-Security-Policy-Report-Only")); diff != "" {
+				t.Errorf("h.Values(\"Content-Security-Policy-Report-Only\") mismatch (-want +got):\n%s", diff)
+			}
+
+			ctx := req.Context()
+			en := ctx.Value(ctxKey("enforce"))
+			if diff := cmp.Diff(tt.wantEnforcementNonces, en); !(en == nil && tt.wantEnforcementNonces == nil) && diff != "" {
+				t.Errorf("ctx.Value(ctxKey(\"enforce\")) mismatch (-want +got):\n%s", diff)
+			}
+
+			rn := ctx.Value(ctxKey("report"))
+			if diff := cmp.Diff(tt.wantReportNonces, rn); !(rn == nil && tt.wantReportNonces == nil) && diff != "" {
+				t.Errorf("ctx.Value(ctxKey(\"report\")) mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestAlreadyClaimed(t *testing.T) {
+	headers := []string{
+		"Content-Security-Policy",
+		"Content-Security-Policy-Report-Only",
+	}
+
+	for _, h := range headers {
+		t.Run(h, func(t *testing.T) {
+			rr := safehttptest.NewResponseRecorder()
+			if _, err := rr.ResponseWriter.Header().Claim(h); err != nil {
+				t.Fatalf("rr.ResponseWriter.Header().Claim(h) got: %v want: nil", err)
+			}
+			req := safehttptest.NewRequest(safehttp.MethodGet, "/", nil)
+
+			it := Interceptor{}
+			it.Before(rr.ResponseWriter, req)
+
+			if got, want := rr.Status(), int(safehttp.StatusInternalServerError); got != want {
+				t.Errorf("rr.Status() got: %v want: %v", got, want)
+			}
+
+			wantHeaders := map[string][]string{
+				"Content-Type":           {"text/plain; charset=utf-8"},
+				"X-Content-Type-Options": {"nosniff"},
+			}
+			if diff := cmp.Diff(wantHeaders, map[string][]string(rr.Header())); diff != "" {
+				t.Errorf("rr.Header() mismatch (-want +got):\n%s", diff)
+			}
+
+			if got, want := rr.Body(), "Internal Server Error\n"; got != want {
+				t.Errorf("rr.Body() got: %q want: %q", got, want)
+			}
+		})
+	}
+}
