f8370b301b60ddd08b4a74ddf256349f0e5e7ddd
Mara Mihali <maramihali@google.com>
2020-08-24 11:14:24 +0300

    Resolved comments from code review

--- safehttp/plugins/fetch_metadata/fetch_metadata.go
+++ safehttp/plugins/fetch_metadata/fetch_metadata.go
@@ -18,12 +18,8 @@ import (
 	"github.com/google/go-safeweb/safehttp"
 )
 
-const (
-	enforceMode = "enforce"
-	reportMode  = "report"
-)
-
-// LoggingService TODO
+// LoggingService is a user-provided service for logging Fetch Metadata policy
+// violations
 type LoggingService interface {
 	Log(ir *safehttp.IncomingRequest)
 }
@@ -31,17 +27,19 @@ type LoggingService interface {
 // Plugin implements Fetch Metadata functionality.
 // See https://www.w3.org/TR/fetch-metadata/ for more details.
 type Plugin struct {
-	mode   string
-	policy func(*safehttp.IncomingRequest) bool
-	logger LoggingService
+	// Policy is the Fetch Metadata policy that decides whether a request should
+	// be allowed to pass or be rejected. This will be set to a default Resource
+	// Isolation Policy by default but can be changed by the user.
+	Policy     func(*safehttp.IncomingRequest) bool
+	reportOnly bool
+	logger     LoggingService
 }
 
 // NewPlugin creates a new Fetch Metadata plugin using the defaultPolicy and
 // sets the mode to enforce.
 func NewPlugin() *Plugin {
 	return &Plugin{
-		mode:   enforceMode,
-		policy: defaultPolicy,
+		Policy: defaultPolicy,
 	}
 }
 
@@ -50,7 +48,8 @@ func NewPlugin() *Plugin {
 //
 // See https://web.dev/fetch-metadata/ for more information.
 func defaultPolicy(r *safehttp.IncomingRequest) bool {
-	switch r.Header.Get("Sec-Fetch-Site") {
+	h := r.Header
+	switch h.Get("Sec-Fetch-Site") {
 	case "":
 		// Fetch Metadata is not supported by the browser so we allow the
 		// request to pass.
@@ -63,8 +62,8 @@ func defaultPolicy(r *safehttp.IncomingRequest) bool {
 		// allowed to pass.
 		return true
 	}
-	if m := r.Method(); r.Header.Get("Sec-Fetch-Mode") == "navigate" && (m == "GET" || m == "HEAD") {
-		if dest := r.Header.Get("Sec-Fetch-Dest"); dest == "object" || dest == "embed" {
+	if m := r.Method(); h.Get("Sec-Fetch-Mode") == "navigate" && (m == "GET" || m == "HEAD") {
+		if dest := h.Get("Sec-Fetch-Dest"); dest == "object" || dest == "embed" {
 			// The request is cross-site and originates from <object> or <embed>
 			// so it is rejected.
 			return false
@@ -77,12 +76,6 @@ func defaultPolicy(r *safehttp.IncomingRequest) bool {
 	return false
 }
 
-// SetPolicy allows changing the default Fetch Metadata policy
-// to a user-provided policy.
-func (p *Plugin) SetPolicy(policy func(*safehttp.IncomingRequest) bool) {
-	p.policy = policy
-}
-
 // SetReportMode sets the Fetch Metadata policy mode to "report". This will
 // allow requests that violate the policy to pass, but will report the violation
 // using the LoggingService. The method will panic if no LoggingService is
@@ -92,13 +85,13 @@ func (p *Plugin) SetReportMode(logger LoggingService) {
 		panic("logging service required for Fetch Metadata report mode")
 	}
 	p.logger = logger
-	p.mode = reportMode
+	p.reportOnly = true
 }
 
 // SetEnforceMode sets the Fetch Metadata policy mode to "enforce". This will
 // reject any requests that violate the policy provided by the plugin.
 func (p *Plugin) SetEnforceMode() {
-	p.mode = enforceMode
+	p.reportOnly = false
 }
 
 // Before validates the safehttp.IncomingRequest using the Fetch Metadata policy
@@ -108,14 +101,13 @@ func (p *Plugin) SetEnforceMode() {
 // Fetch Metadata support implemented, the policy will not be applied and all
 // requests will be allowed to pass.
 func (p *Plugin) Before(w safehttp.ResponseWriter, r *safehttp.IncomingRequest) safehttp.Result {
-	if !p.policy(r) {
-		switch p.mode {
-		case enforceMode:
+	if !p.Policy(r) {
+		switch p.reportOnly {
+		case false:
 			return w.ClientError(safehttp.StatusForbidden)
-		case reportMode:
+		case true:
 			p.logger.Log(r)
 		}
 	}
-
 	return safehttp.Result{}
 }
