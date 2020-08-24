34e3ff67b7d4956255ed817be618a4fb99a3494d
Mattias Grenfeldt <mattiasgrenfeldt@gmail.com>
2020-08-24 11:10:32 +0200

    Added descriptions of Report fields, copied from MDN.

--- safehttp/plugins/collector/collector.go
+++ safehttp/plugins/collector/collector.go
@@ -23,15 +23,31 @@ import (
 // Report represents a CSP violation report as specified by
 // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only#Violation_report_syntax
 type Report struct {
-	BlockedURI         string `json:"blocked-uri"`
-	Disposition        string `json:"disposition"`
-	DocumentURI        string `json:"document-uri"`
+	// BlockedURI is the URI of the resource that was blocked from loading by the
+	// Content Security Policy. If the blocked URI is from a different origin than
+	// the DocumentURI, then the blocked URI is truncated to contain just the scheme,
+	// host, and port.
+	BlockedURI string `json:"blocked-uri"`
+	// Disposition is either "enforce" or "report" depending on whether the Content-Security-Policy
+	// header or the Content-Security-Policy-Report-Only header is used.
+	Disposition string `json:"disposition"`
+	// DocumentURI is the URI of the document in which the violation occurred.
+	DocumentURI string `json:"document-uri"`
+	// EffectiveDirective is the directive whose enforcement caused the violation.
 	EffectiveDirective string `json:"effective-directive"`
-	OriginalPolicy     string `json:"original-policy"`
-	Referrer           string `json:"referrer"`
-	ScriptSample       string `json:"script-sample"`
-	StatusCode         int    `json:"status-code"`
-	ViolatedDirective  string `json:"violated-directive"`
+	// OriginalPolicy is the original policy as specified by the Content Security
+	// Policy header.
+	OriginalPolicy string `json:"original-policy"`
+	// Referrer is the referrer of the document in which the violation occurred.
+	Referrer string `json:"referrer"`
+	// ScriptSample is the first 40 characters of the inline script, event handler,
+	// or style that caused the violation.
+	ScriptSample string `json:"script-sample"`
+	// StatusCode the HTTP status code of the resource on which the global object
+	// was instantiated.
+	StatusCode int `json:"status-code"`
+	// ViolatedDirective is the name of the policy section that was violated.
+	ViolatedDirective string `json:"violated-directive"`
 }
 
 // ReportsHandler is a reports handler.
