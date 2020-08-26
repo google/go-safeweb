c2f57d9711645827c60803dc3328e8e06c39b844
Mara Mihali <maramihali@google.com>
2020-08-26 13:13:18 +0300

    Added all methods supported by net/http

--- safehttp/mux.go
+++ safehttp/mux.go
@@ -18,12 +18,16 @@ import (
 	"net/http"
 )
 
-// TODO: add the missing methods
 const (
-	// HTTP GET request
-	MethodGet = "GET"
-	// HTTP Post request
-	MethodPost = "POST"
+	MethodGet     = "GET"     // RFC 7231, 4.3.1
+	MethodHead    = "HEAD"    // RFC 7231, 4.3.2
+	MethodPost    = "POST"    // RFC 7231, 4.3.3
+	MethodPut     = "PUT"     // RFC 7231, 4.3.4
+	MethodDelete  = "DELETE"  // RFC 7231, 4.3.5
+	MethodConnect = "CONNECT" // RFC 7231, 4.3.6
+	MethodOptions = "OPTIONS" // RFC 7231, 4.3.7
+	MethodTrace   = "TRACE"   // RFC 7231, 4.3.8
+	MethodPatch   = "PATCH"   // RFC 5789
 )
 
 // ServeMux is an HTTP request multiplexer. It matches the URL of each incoming
