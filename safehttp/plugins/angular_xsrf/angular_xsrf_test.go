523a9ec7e2c86024b660a991cec76300c6cba525
Mara Mihali <maramihali@google.com>
2020-10-14 14:22:02 +0300

    Implemented support for Angular's XHR XSRF protection

---
+++ safehttp/plugins/angular_xsrf/angular_xsrf_test.go
@@ -0,0 +1,147 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package xsrf provides a safehttp.Interceptor that ensures Cross-Site Request
+// Forgery protection by verifying the incoming requests, rejecting those
+// requests that are suspected to be part of an attack.
+package angularxsrf
+
+import (
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-safeweb/safehttp"
+	"github.com/google/go-safeweb/safehttp/safehttptest"
+	"strings"
+	"testing"
+)
+
+const (
+	cookieName = "XSRF-TOKEN"
+	headerName = "X-XSRF-TOKEN"
+)
+
+func TestAngularAddCookie(t *testing.T) {
+	req := safehttptest.NewRequest(safehttp.MethodGet, "/", nil)
+	rec := safehttptest.NewResponseRecorder()
+	i := &Interceptor{
+		TokenCookieName: cookieName,
+		TokenHeaderName: headerName,
+	}
+	i.Commit(rec.ResponseWriter, req, nil, nil)
+
+	if got, want := rec.Status(), safehttp.StatusOK; got != want {
+		t.Errorf("rec.Status(): got %v, want %v", got, want)
+	}
+
+	tokCookieDefaults := "Path=/; Max-Age=86400; Secure; SameSite=Strict"
+	got := map[string][]string(rec.Header())["Set-Cookie"][0]
+	if !strings.Contains(got, i.TokenCookieName) {
+		t.Errorf("Set-Cookie header: %s not present", i.TokenCookieName)
+	}
+	if !strings.Contains(got, tokCookieDefaults) {
+		t.Errorf("Set-Cookie header: got %s, want defaults %s", got, tokCookieDefaults)
+	}
+
+	if got, want := rec.Body(), ""; got != want {
+		t.Errorf("rec.Body(): got %q want %q", got, want)
+	}
+}
+
+func TestAngularPostProtection(t *testing.T) {
+	tests := []struct {
+		name       string
+		req        *safehttp.IncomingRequest
+		wantStatus safehttp.StatusCode
+		wantHeader map[string][]string
+		wantBody   string
+	}{
+		{
+			name: "Same cookie and header",
+			req: func() *safehttp.IncomingRequest {
+				req := safehttptest.NewRequest(safehttp.MethodPost, "/", nil)
+				req.Header.Set("Cookie", cookieName+"="+"1234")
+				req.Header.Set(headerName, "1234")
+				return req
+			}(),
+			wantStatus: safehttp.StatusOK,
+			wantHeader: map[string][]string{},
+			wantBody:   "",
+		},
+		{
+			name: "Different cookie and header",
+			req: func() *safehttp.IncomingRequest {
+				req := safehttptest.NewRequest(safehttp.MethodPost, "/", nil)
+				req.Header.Set("Cookie", cookieName+"="+"5768")
+				req.Header.Set(headerName, "1234")
+				return req
+			}(),
+			wantStatus: safehttp.StatusUnauthorized,
+			wantHeader: map[string][]string{
+				"Content-Type":           {"text/plain; charset=utf-8"},
+				"X-Content-Type-Options": {"nosniff"},
+			},
+			wantBody: "Unauthorized\n",
+		},
+		{
+			name: "Missing header",
+			req: func() *safehttp.IncomingRequest {
+				req := safehttptest.NewRequest(safehttp.MethodPost, "/", nil)
+				req.Header.Set("Cookie", cookieName+"="+"1234")
+				return req
+			}(),
+			wantStatus: safehttp.StatusUnauthorized,
+			wantHeader: map[string][]string{
+				"Content-Type":           {"text/plain; charset=utf-8"},
+				"X-Content-Type-Options": {"nosniff"},
+			},
+			wantBody: "Unauthorized\n",
+		},
+		{
+			name: "Missing cookie",
+			req: func() *safehttp.IncomingRequest {
+				req := safehttptest.NewRequest(safehttp.MethodPost, "/", nil)
+				req.Header.Set(headerName, "1234")
+				return req
+			}(),
+			wantStatus: safehttp.StatusForbidden,
+			wantHeader: map[string][]string{
+				"Content-Type":           {"text/plain; charset=utf-8"},
+				"X-Content-Type-Options": {"nosniff"},
+			},
+			wantBody: "Forbidden\n",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			rec := safehttptest.NewResponseRecorder()
+			i := &Interceptor{
+				TokenCookieName: cookieName,
+				TokenHeaderName: headerName,
+			}
+			i.Before(rec.ResponseWriter, test.req, nil)
+
+			if got := rec.Status(); got != test.wantStatus {
+				t.Errorf("rec.Status(): got %v, want %v", got, test.wantStatus)
+			}
+
+			if diff := cmp.Diff(test.wantHeader, map[string][]string(rec.Header())); diff != "" {
+				t.Errorf("rec.Header() mismatch (-want +got):\n%s", diff)
+			}
+
+			if got := rec.Body(); got != test.wantBody {
+				t.Errorf("rec.Body(): got %q want %q", got, test.wantBody)
+			}
+		})
+	}
+}
