23e5f90e4e4b0c020a3b88be5aa01eedeb0be93f
Mara Mihali <maramihali@google.com>
2020-10-14 16:55:26 +0300

    Ensure the XSRF plugins satisfy the safehttp.Interceptor interface by adding the OnError methods

--- safehttp/plugins/angular_xsrf/angular_xsrf.go
+++ safehttp/plugins/angular_xsrf/angular_xsrf.go
@@ -112,3 +112,8 @@ func (it *Interceptor) Commit(w *safehttp.ResponseWriter, r *safehttp.IncomingRe
 	}
 	return safehttp.NotWritten()
 }
+
+// OnError is a no-op, required to satisfy the safehttp.Interceptor interface.
+func (it *Interceptor) OnError(w *safehttp.ResponseWriter, r *safehttp.IncomingRequest, resp safehttp.Response, _ safehttp.InterceptorConfig) safehttp.Result {
+	return safehttp.NotWritten()
+}
