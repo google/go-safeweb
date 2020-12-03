fe978eb728027eb1fa31c75916ecc0dc44026d73
Damian Bogel <kele@google.com>
2020-12-03 13:13:28 +0100

    Added hsts testcase covering error responses.

--- tests/integration/hsts/hsts_test.go
+++ tests/integration/hsts/hsts_test.go
@@ -60,3 +60,27 @@ func TestHSTSServeMuxInstall(t *testing.T) {
 		t.Errorf("b.String() got: %v want: %v", got, want)
 	}
 }
+
+func TestHSTSOnErrors(t *testing.T) {
+	mb := &safehttp.ServeMuxConfig{}
+
+	mb.Intercept(hsts.Default())
+	handler := safehttp.HandlerFunc(func(w safehttp.ResponseWriter, r *safehttp.IncomingRequest) safehttp.Result {
+		return w.WriteError(safehttp.StatusNotFound)
+	})
+	mb.Handle("/asdf", safehttp.MethodGet, handler)
+
+	b := strings.Builder{}
+	rw := safehttptest.NewTestResponseWriter(&b)
+
+	req := httptest.NewRequest(http.MethodGet, "https://foo.com/asdf", nil)
+
+	mb.Mux().ServeHTTP(rw, req)
+
+	if want := safehttp.StatusNotFound; rw.Status() != want {
+		t.Errorf("rw.Status() got: %v want: %v", rw.Status(), want)
+	}
+	if got, want := rw.Header()["Strict-Transport-Security"], []string{"max-age=63072000; includeSubDomains"}; !cmp.Equal(got, want) {
+		t.Errorf("rw.Header()[\"Strict-Transport-Security\"] = %q, want %q", got, want)
+	}
+}
