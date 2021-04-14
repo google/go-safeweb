baf7fb8b8681ede0154b8679798477d049ab0788
Damian Bogel <kele@google.com>
2021-04-14 10:55:09 +0200

    Replaced safehttptest with httptest in fileserver_test.go

--- safehttp/fileserver_test.go
+++ safehttp/fileserver_test.go
@@ -18,12 +18,10 @@ import (
 	"io/ioutil"
 	"net/http/httptest"
 	"os"
-	"strings"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-safeweb/safehttp"
-	"github.com/google/go-safeweb/safehttp/safehttptest"
 )
 
 func TestFileServer(t *testing.T) {
@@ -47,14 +45,14 @@ func TestFileServer(t *testing.T) {
 		{
 			name:     "missing file",
 			path:     "failure",
-			wantCode: 404,
+			wantCode: safehttp.StatusNotFound,
 			wantCT:   "text/plain; charset=utf-8",
 			wantBody: "Not Found\n",
 		},
 		{
 			name:     "file",
 			path:     "foo.html",
-			wantCode: 200,
+			wantCode: safehttp.StatusOK,
 			wantCT:   "text/html; charset=utf-8",
 			wantBody: "<h1>Hello world</h1>",
 		},
@@ -66,19 +64,18 @@ func TestFileServer(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			var b strings.Builder
-			rr := safehttptest.NewTestResponseWriter(&b)
+			rr := httptest.NewRecorder()
 
 			req := httptest.NewRequest(safehttp.MethodGet, "https://test.science/"+tt.path, nil)
 			m.ServeHTTP(rr, req)
 
-			if got, want := rr.Status(), tt.wantCode; got != tt.wantCode {
+			if got, want := rr.Code, tt.wantCode; got != int(tt.wantCode) {
 				t.Errorf("status code got: %v want: %v", got, want)
 			}
 			if got := rr.Header().Get("Content-Type"); tt.wantCT != got {
 				t.Errorf("Content-Type: got %q want %q", got, tt.wantCT)
 			}
-			if diff := cmp.Diff(tt.wantBody, b.String()); diff != "" {
+			if diff := cmp.Diff(tt.wantBody, rr.Body.String()); diff != "" {
 				t.Errorf("Response body diff (-want,+got): \n%s", diff)
 			}
 		})
