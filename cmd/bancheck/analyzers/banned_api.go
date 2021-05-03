28765a805a38d222f7881c413f22aa83985fe677
Maciej Trzos <maciek.trzos@gmail.com>
2021-05-03 17:22:24 +0200

    Unify banned API checkers into one file.

---
+++ cmd/bancheck/analyzers/banned_api.go
@@ -0,0 +1,133 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package analyzers
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"go/token"
+	"go/types"
+	"path/filepath"
+	"strings"
+
+	"github.com/google/go-safeweb/cmd/bancheck/config"
+	"golang.org/x/tools/go/analysis"
+)
+
+// NewBannedAPIAnalyzer returns an analyzer that checks for usage of banned APIs.
+func NewBannedAPIAnalyzer() *analysis.Analyzer {
+	fs := flag.NewFlagSet("", flag.ExitOnError)
+	fs.String("config", "", "Config files with banned APIs separated by a comma")
+
+	a := &analysis.Analyzer{
+		Name:  "bannedAPI",
+		Doc:   "Checks for usage of banned APIs",
+		Run:   checkBannedAPIs,
+		Flags: *fs,
+	}
+
+	return a
+}
+
+func checkBannedAPIs(pass *analysis.Pass) (interface{}, error) {
+	cfgFiles := pass.Analyzer.Flags.Lookup("config").Value.(flag.Getter).Get().(string)
+	if cfgFiles == "" {
+		return nil, errors.New("please, provide a config file")
+	}
+
+	cfg, err := config.ReadConfigs(strings.Split(cfgFiles, ","))
+	if err != nil {
+		return nil, err
+	}
+
+	checkBannedImports(pass, bannedAPIMap(cfg.Imports))
+	checkBannedFunctions(pass, bannedAPIMap(cfg.Functions))
+
+	return nil, nil
+}
+
+func checkBannedImports(pass *analysis.Pass, bannedImports map[string][]config.BannedAPI) (interface{}, error) {
+	for _, f := range pass.Files {
+		for _, i := range f.Imports {
+			importName := strings.Trim(i.Path.Value, "\"")
+			err := reportIfBanned(importName, bannedImports, i.Pos(), pass)
+			if err != nil {
+				return false, err
+			}
+		}
+	}
+	return nil, nil
+}
+
+func checkBannedFunctions(pass *analysis.Pass, bannedFns map[string][]config.BannedAPI) (interface{}, error) {
+	for id, obj := range pass.TypesInfo.Uses {
+		fn, ok := obj.(*types.Func)
+		if !ok {
+			continue
+		}
+
+		fnName := fmt.Sprintf("%s.%s", fn.Pkg().Path(), fn.Name())
+		err := reportIfBanned(fnName, bannedFns, id.Pos(), pass)
+		if err != nil {
+			return false, err
+		}
+	}
+	return nil, nil
+}
+
+func reportIfBanned(APIname string, bannedAPIs map[string][]config.BannedAPI, position token.Pos, pass *analysis.Pass) error {
+	if bannedAPICfgs, isBanned := bannedAPIs[APIname]; isBanned {
+		pkgAllowed, err := isPkgAllowed(pass.Pkg, bannedAPICfgs)
+		if err != nil {
+			return err
+		}
+
+		if !pkgAllowed {
+			for _, bannedAPICfg := range bannedAPICfgs {
+				pass.Report(analysis.Diagnostic{
+					Pos:     position,
+					Message: fmt.Sprintf("Banned API found %q. Additional info: %s", APIname, bannedAPICfg.Msg),
+				})
+			}
+		}
+	}
+	return nil
+}
+
+// isPkgAllowed checks if the Go package should be exempted from reporting banned API usages.
+func isPkgAllowed(pkg *types.Package, bannedAPI []config.BannedAPI) (bool, error) {
+	for _, fn := range bannedAPI {
+		for _, e := range fn.Exemptions {
+			match, err := filepath.Match(e.AllowedPkg, pkg.Path())
+			if err != nil {
+				return false, err
+			}
+			if match {
+				return true, nil
+			}
+		}
+	}
+	return false, nil
+}
+
+// bannedAPIMap builds a mapping of fully qualified API name to a list of
+// all its config.BannedAPI entries.
+func bannedAPIMap(bannedAPIs []config.BannedAPI) map[string][]config.BannedAPI {
+	m := make(map[string][]config.BannedAPI)
+	for _, API := range bannedAPIs {
+		m[API.Name] = append(m[API.Name], API)
+	}
+	return m
+}
---
+++ cmd/bancheck/analyzers/banned_api_test.go
@@ -0,0 +1,229 @@
+package analyzers
+
+import (
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"golang.org/x/tools/go/analysis/analysistest"
+)
+
+func TestBannedAPIAnalyzer(t *testing.T) {
+	for _, test := range []struct {
+		desc  string
+		files map[string]string
+	}{
+		{
+			desc: "No banned APIs",
+			files: map[string]string{
+				"config.json": `
+				{}
+				`,
+				"main/test.go": `
+				package main;
+				func main() {}
+				`,
+			},
+		},
+		{
+			desc: "Banned APIs exist",
+			files: map[string]string{
+				"config.json": `
+				{
+					"functions": [
+						{
+							"name": "fmt.Printf",
+							"msg": "Banned by team A"
+						}
+					],
+					"imports": [
+						{
+							"name": "fmt",
+							"msg": "Banned by team A"
+						}
+					]
+				}
+				`,
+				"main/test.go": `
+				package main
+
+				import "fmt" // want "Banned API found \"fmt\". Additional info: Banned by team A"
+
+				func main() {
+					fmt.Printf("Hello") // want "Banned API found \"fmt.Printf\". Additional info: Banned by team A"
+				}
+				`,
+			},
+		},
+		{
+			desc: "Banned APIs in exempted package",
+			files: map[string]string{
+				"config.json": `
+				{
+					"functions": [
+						{
+							"name": "fmt.Printf",
+							"msg": "Banned by team A",
+							"exemptions": [
+								{
+									"justification": "#yolo",
+									"allowedPkg": "main"
+								}
+							]
+						}
+					],
+					"imports": [
+						{
+							"name": "fmt",
+							"msg": "Banned by team A",
+							"exemptions": [
+								{
+									"justification": "#yolo",
+									"allowedPkg": "main"
+								}
+							]
+						}
+					]
+				}
+				`,
+				"main/test.go": `
+				package main
+
+				import "fmt"
+
+				func main() {
+					fmt.Printf("Hello")
+				}
+				`,
+			},
+		},
+		{
+			desc: "Banned renamed import",
+			files: map[string]string{
+				"config.json": `
+				{
+					"imports": [
+						{
+							"name": "fmt",
+							"msg": "Banned by team A"
+						}
+					]
+				}
+				`,
+				"main/test.go": `
+				package main
+
+				import allowed "fmt" // want "Banned API found \"fmt\". Additional info: Banned by team A"
+
+				func main() {
+					allowed.Printf("Hello")
+				}
+				`,
+			},
+		},
+		{
+			desc: "Banned function from renamed import",
+			files: map[string]string{
+				"config.json": `
+				{
+					"functions": [
+						{
+							"name": "fmt.Printf",
+							"msg": "Banned by team A"
+						}
+					]
+				}
+				`,
+				"main/test.go": `
+				package main
+
+				import allowed "fmt"
+
+				func main() {
+					allowed.Printf("Hello") // want "Banned API found \"fmt.Printf\". Additional info: Banned by team A"
+				}
+				`,
+			},
+		},
+		{
+			desc: "Package and function name collission",
+			files: map[string]string{
+				"config.json": `
+				{
+					"functions": [
+						{
+							"name": "fmt.Printf",
+							"msg": "Banned by team A"
+						}
+					]
+				}
+				`,
+				"main/test.go": `
+				package main
+
+				type Foo struct{}
+
+				func (f *Foo) Printf(txt string) {}
+
+				func main() {
+					var fmt = &Foo{}
+					fmt.Printf("Hello")
+				}
+				`,
+			},
+		},
+		{
+			desc: "Banned API from multiple config files",
+			files: map[string]string{
+				"team_a_config.json": `
+				{
+					"functions": [
+						{
+							"name": "fmt.Printf",
+							"msg": "Banned by team A"
+						}
+					]
+				}
+				`,
+				"team_b_config.json": `
+				{
+					"functions": [
+						{
+							"name": "fmt.Printf",
+							"msg": "Banned by team B"
+						}
+					]
+				}
+				`,
+				"main/test.go": `
+				package main
+
+				import "fmt"
+
+				func main() {
+					fmt.Printf("Hello") // want "Banned API found \"fmt.Printf\". Additional info: Banned by team A" "Banned API found \"fmt.Printf\". Additional info: Banned by team B"
+				}
+				`,
+			},
+		},
+	} {
+		t.Run(test.desc, func(t *testing.T) {
+			dir, cleanup, err := analysistest.WriteFiles(test.files)
+			if err != nil {
+				t.Fatalf("WriteFiles() returned err: %v", err)
+			}
+			defer cleanup()
+
+			var configFiles []string
+			for name := range test.files {
+				if strings.HasSuffix(name, "config.json") {
+					configFiles = append(configFiles, filepath.Join(dir, "src", name))
+				}
+			}
+
+			a := NewBannedAPIAnalyzer()
+			a.Flags.Set("config", strings.Join(configFiles, ","))
+			analysistest.Run(t, dir, a, "main")
+		})
+	}
+}
