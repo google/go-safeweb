eb79df54b8bc1910ac3bc3fc3328da7c0fb016e1
Benny Siegert <bsiegert@google.com>
2022-01-17 11:15:50 +0000

    all: update build tags
    Running gofmt adds these "new-style" build tags now. This is what
    has caused CI to be red.

--- examples/sample-application/secure/docs.go
+++ examples/sample-application/secure/docs.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//go:build go1.16
 // +build go1.16
 
 // Package secure TODO(clap|kele): describe the assumptions we are under, e.g.
