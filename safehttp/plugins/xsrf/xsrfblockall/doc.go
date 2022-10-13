ac4182bb75e49dd082fb2c3fbd0b2dc010e69b15
Alessio Pragliola <alessio.pragliola@sighup.io>
2022-10-13 11:35:14 +0200

    fix: post review comments

--- safehttp/plugins/xsrf/xsrfblockall/doc.go
+++ safehttp/plugins/xsrf/xsrfblockall/doc.go
@@ -1,4 +1,4 @@
-// Copyright 2020 Google LLC
+// Copyright 2022 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,6 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package xsrfblockall provides a safehttp.Interceptor that ensures Cross-Site
-// Request Forgery by rejecting every state-changing request.
+// Package xsrfblockall provides a safehttp.Interceptor that protects against
+// Cross-Site Request Forgery by rejecting all state-changing request.
 package xsrfblockall
